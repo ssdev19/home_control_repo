5ac2462a28a752e7769f71fbcf99854f44f35843
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-08 13:57:43 -0700

    added timezone

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -5,11 +5,10 @@
 #   Enable or disable metrics collection. Metrics collection may be disabled on development
 #   nodes, nodes that don't have uptime requirements, or nodes that should only have minimal
 #   software load.
-class profile::core::common #(
-#   Boolean $collect_metrics = true,
-# ) 
-{
-#  include timezone
+class profile::core::common(
+  Boolean $collect_metrics = true,
+){
+  include timezone
   # include tuned
   # include chrony
   # include selinux
