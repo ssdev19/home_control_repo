d740dc42c257126f398bef7ef0f943d415dd9f62
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-08 19:10:20 -0700

    df

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -1,7 +1,8 @@
 class profile::core::puppet_master {
   file{ '/root/README':
     ensure  => file,
-    content => "Welcome to the ${fqdn},\n BIOS release date:${bios_release_date} \nthis is a Puppet Master Server\n",
+    content => "Welcome to the ${fqdn},\n BIOS release date:${bios_release_date} \nthis is a Puppet Master Server\n
+    This file is created becaus in foreman role is set to forman",
   }
 include r10k
 }
