76fea641791df43ba1cf2faded1a0d907f47947e
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-08 19:03:44 -0700

    s

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -1,7 +1,7 @@
 class profile::core::puppet_master {
   file{ '/root/README':
     ensure  => file,
-    content => "Welcome to ${fqdn},\n BIOS release date:${bios_release_date} \nthis is a Puppet Master Server\n",
+    content => "Welcome to the ${fqdn},\n BIOS release date:${bios_release_date} \nthis is a Puppet Master Server\n",
   }
 include r10k
 }
