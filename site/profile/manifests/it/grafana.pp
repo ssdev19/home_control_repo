efc9431e8cdd7c06959452bdd688fea4ed6fc7e1
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-10 10:40:52 -0700

    changed grafana ver

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -1,12 +1,12 @@
 class profile::it::grafana {
-	class { 'grafana': 
-		version => '6.1.0'
-	}
+  class { 'grafana': 
+    version  => '8.0.0'
+  }
 
-	firewalld_port { 'Grafana Main Port':
-		ensure   => present,
-		port     => '3000',
-		protocol => 'tcp',
-		require => Service['firewalld'],
-	}
+  firewalld_port { 'Grafana Main Port':
+    ensure   => present,
+    port     => '3000',
+    protocol => 'tcp',
+    require  => Service['firewalld'],
+  }
 }
