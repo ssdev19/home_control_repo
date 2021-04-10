29ff9bc07d1bbf7ee4530171ca85096dac33af2e
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-10 15:28:18 -0700

    qa

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -1,4 +1,16 @@
 class profile::it::influxdb {
+
+# create certs for http
+  $openssl_country = lookup('country')
+  $openssl_state = lookup("state")
+  $openssl_locality = lookup("locality")
+  $openssl_cn = $trusted['certname']
+
+  exec{"Create Selfsigned cert":
+    path    => "/usr/bin/",
+    command => "openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/ssl/influxdb.key -out /etc/ssl/influxdb.pem -days 365 -subj \"/C=${openssl_country}/ST=${openssl_state}/L=${openssl_locality}/O=LSST/CN=${openssl_cn}\"",
+    onlyif  => "test ! -f /etc/ssl/influxdb.pem"
+  }
   class {'influxdb':
     # ensure                 => present,
     service_ensure              => running,
@@ -13,20 +25,12 @@ class profile::it::influxdb {
     # influxd_opts           => lookup('influxdb_opts'),
     http_https_enabled          => true,
     http_https_certificate_path => "/etc/ssl/influxdb.pem",
+    http_https_certificate_content => lookup 
     http_https_private_key_path => "/etc/ssl/influxdb.key",
     admin_password              => lookup('influx_admin_user'),
     }
 
-  $openssl_country = lookup('country')
-  $openssl_state = lookup("state")
-  $openssl_locality = lookup("locality")
-  $openssl_cn = $trusted['certname']
 
-  exec{"Create Selfsigned cert":
-    path    => "/usr/bin/",
-    command => "openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/ssl/influxdb.key -out /etc/ssl/influxdb.pem -days 365 -subj \"/C=${openssl_country}/ST=${openssl_state}/L=${openssl_locality}/O=LSST/CN=${openssl_cn}\"",
-    onlyif  => "test ! -f /etc/ssl/influxdb.pem"
-  }
 
   firewalld_port { 'InfluxDB Main Port':
     ensure   => present,
