583a31862dbedf1d8fdec4d7b62dbea84995910f
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-11 08:34:26 -0700

    d

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -41,7 +41,7 @@ class profile::it::influxdb {
   $influx_telegraf_passwd = lookup('influx_telegraf_passwd')
   $influx_telegraf_db_name = lookup('influx_telegraf_db_name')
 
-influx_user{$influx_admin_user:
+influx_telegraf_user{$influx_admin_user:
     ensure   => present,
     password => $influx_admin_passwd,
     database => $influx_telegraf_db_name,
