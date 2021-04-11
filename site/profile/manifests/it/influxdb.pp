6d9a58f752208384d8208a5ed107a981e6e6967f
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-10 17:39:56 -0700

    aa

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -30,7 +30,7 @@ class profile::it::influxdb {
     http_https_certificate_path => '/etc/ssl/influxdb.pem',
   #  http_https_certificate_content => lookup 
     http_https_private_key_path => '/etc/ssl/influxdb.key',
-    admin_user                  => lookup(influx_admin_password),
+    admin_user                  => lookup('influx_admin_user'),
     admin_password              => lookup('influx_admin_password'),
     }
 
