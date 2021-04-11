b601895613c150e64ac2b05afcfc9a1391f8ef18
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-11 12:38:59 -0700

    za

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -23,7 +23,7 @@ class profile::it::influxdb {
 #    log_enabled                 => true,
 #    suppress-write-log          => false,
   #  pprof_enabled               => true,
-    bind_address                   => ':8088',
+    bind_address                   => 'influxdb.home.vm:8088',
     # meta_http_bind_address => ":8091",
     #bind_address                => ':8086',
     # influxd_opts           => lookup('influxdb_opts'),
