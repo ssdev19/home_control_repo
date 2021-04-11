43e475cd208d3d642f9efec28d1bf60634f36bef
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-11 13:20:34 -0700

    xx

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -85,9 +85,9 @@ class profile::it::influxdb {
   # }
 
 
-	exec{"Create telegraf database on influxdb":
-		path    => ['/usr/bin','/usr/sbin'],
-		command => "influx -ssl -unsafeSsl -username '${influx_admin_user}' -password '${influx_admin_passwd}' -execute \"CREATE DATABASE ${influx_telegraf_db_name}\"",
+	# exec{"Create telegraf database on influxdb":
+	# 	path    => ['/usr/bin','/usr/sbin'],
+	# 	command => "influx -ssl -unsafeSsl -username '${influx_admin_user}' -password '${influx_admin_passwd}' -execute \"CREATE DATABASE ${influx_telegraf_db_name}\"",
 		# require => Exec["Create admin user on influxdb"],
 		# onlyif  => "test $(influx -ssl -unsafeSsl -username '${influx_admin_user}' -password '${influx_admin_passwd}' -execute \"SHOW DATABASES\" | grep ${influx_telegraf_db_name} | wc -l ) -lt 1"
 	}
