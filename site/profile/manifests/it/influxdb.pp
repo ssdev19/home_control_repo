38d93a59149e50ed6264a280208f12037dacd5fa
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-11 17:51:46 -0700

    cc

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -48,16 +48,16 @@ class profile::it::influxdb {
       fail('If you enable https you must provide certificate and key')
     }
 
-    if $facts['os']['family'] == 'Debian' {
-      # TODO: convert to ruby
-      exec {
-        'Add influxdb to ssl-cert group':
-          user    => 'root',
-          command => 'gpasswd -a influxdb ssl-cert',
-          unless  => "id influxdb | grep '(ssl-cert)'",
-          path    => ['/bin', '/usr/bin'];
-      }
-    }
+    # if $facts['os']['family'] == 'Debian' {
+    #   # TODO: convert to ruby
+    #   exec {
+    #     'Add influxdb to ssl-cert group':
+    #       user    => 'root',
+    #       command => 'gpasswd -a influxdb ssl-cert',
+    #       unless  => "id influxdb | grep '(ssl-cert)'",
+    #       path    => ['/bin', '/usr/bin'];
+    #   }
+    # }
 
     if $influxdb::manage_ssl_certs {
       file {
