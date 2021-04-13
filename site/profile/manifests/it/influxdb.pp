7204022129d76465db82f56856fc662c17e8202d
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-13 14:34:04 -0700

    aa

--- site/profile/manifests/it/influxdb.pp
+++ site/profile/manifests/it/influxdb.pp
@@ -36,7 +36,7 @@ class profile::it::influxdb {
     bind_address                   => ':8088',
 #     # meta_http_bind_address => ":8091",
     http_bind_address               => ':8086',
-#     # influxd_opts           => lookup('influxdb_opts'),
+    influxd_opts           => lookup('influxdb_opts'),
     http_https_certificate_path    => '/etc/ssl/influxdb.crt',
 #    http_https_certificate_content => lookup('https_certificate_content'),
     http_https_private_key_path    => '/etc/ssl/influxdb.key',
