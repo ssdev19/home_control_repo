0da95f4ccc8813eec83faff2d4f3b8eb6a5bfec8
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-15 09:55:45 -0700

    disable firewall

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -20,36 +20,36 @@ include ssh
 	}  
 	$lsst_firewall_default_zone = lookup("lsst_firewall_default_zone")
 
-	class { "firewalld":
-		service_ensure => lookup("firewalld_status"),
-		default_zone => $lsst_firewall_default_zone,
-	}
+# 	class { "firewalld":
+# 		service_ensure => lookup("firewalld_status"),
+# 		default_zone => $lsst_firewall_default_zone,
+# 	}
 
-	firewalld_zone { $lsst_firewall_default_zone:
-		ensure => present,
-		target => lookup("lsst_firewall_default_target"),
-		sources => lookup("lsst_firewall_default_sources")
-	}
+# 	firewalld_zone { $lsst_firewall_default_zone:
+# 		ensure => present,
+# 		target => lookup("lsst_firewall_default_target"),
+# 		sources => lookup("lsst_firewall_default_sources")
+# 	}
 
-	firewalld_service { 'Enable SSH':
-		ensure  => 'present',
-		service => 'ssh',
-	}
+# 	firewalld_service { 'Enable SSH':
+# 		ensure  => 'present',
+# 		service => 'ssh',
+# 	}
 
-	firewalld_service { 'Enable DHCP':
-		ensure  => 'present',
-		service => 'dhcpv6-client',
-	}
+# 	firewalld_service { 'Enable DHCP':
+# 		ensure  => 'present',
+# 		service => 'dhcpv6-client',
+# 	}
 
-	exec{"enable_icmp":
-		provider => "shell",
-		command => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",
-		require => Class["firewalld"],
-		onlyif => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
-	}
-	Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
-'acpid', 'wget', 'nmap']:
-ensure => installed,
-}
+# 	exec{"enable_icmp":
+# 		provider => "shell",
+# 		command => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",
+# 		require => Class["firewalld"],
+# 		onlyif => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
+# 	}
+# 	Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
+# 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
+# 'acpid', 'wget', 'nmap']:
+# ensure => installed,
+# }
 }
