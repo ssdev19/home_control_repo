a5447f1102698f4ee59c4bb585c2881a31fec08f
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-15 07:48:30 -0700

    xc

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -1,8 +1,13 @@
 class profile::core::puppet_master {
+package { 'toml':
+  ensure   => 'installed',
+  provider => 'puppetserver_gem',
+}
   file{ '/root/README':
     ensure  => file,
     content => "Welcome to the ${fqdn},\n BIOS release date:${bios_release_date} \nthis is a Puppet Master Server\n
     This file is created because profile::core::foreman includes this class and in foreman role is set to foreman",
   }
 include r10k
+
 }
