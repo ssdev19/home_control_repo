154ebf0dc9a8d4891f7122a1937b3c20ceec56e8
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-22 08:36:25 -0700

    az

--- site/profile/manifests/it/influxdb2.pp
+++ site/profile/manifests/it/influxdb2.pp
@@ -46,9 +46,9 @@ class { '::influxdb':
     #   database   => $influx_telegraf_db_name,
     # },
     'telegraftest' => {
-      # 'password'  => 'influx_telegraf_passwd',
-      # 'privilege' => 'READ',
-      # 'database'  => 'telegraf',
+      'password'  => 'influx_telegraf_passwd',
+      'privilege' => 'READ',
+      'database'  => 'telegraf',
       'ensure'    => absent,
     },
   },
