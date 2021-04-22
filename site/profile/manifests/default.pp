9eae7a6f5380724c73869bef7dad75c5106cb898
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-22 10:52:07 -0700

    az

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -13,40 +13,40 @@ include ssh
 
   # package { $enhancers: }
   # Firewall and security measurements
-	file_line { 'SELINUX=permissive':
-		path  => '/etc/selinux/config',
-		line  => 'SELINUX=enforce',
-		match => '^SELINUX=+',
-	}  
-	$lsst_firewall_default_zone = lookup("lsst_firewall_default_zone")
+  file_line { 'SELINUX=permissive':
+    path  => '/etc/selinux/config',
+    line  => 'SELINUX=enforce',
+    match => '^SELINUX=+',
+  }
+  $lsst_firewall_default_zone = lookup("lsst_firewall_default_zone")
 
-	class { "firewalld":
-		service_ensure => lookup("firewalld_status"),
-		default_zone => $lsst_firewall_default_zone,
-	}
+  class { "firewalld":
+    service_ensure => lookup("firewalld_status"),
+    default_zone   => $lsst_firewall_default_zone,
+  }
 
-	firewalld_zone { $lsst_firewall_default_zone:
-		ensure => present,
-		target => lookup("lsst_firewall_default_target"),
-		sources => lookup("lsst_firewall_default_sources")
-	}
+  firewalld_zone { $lsst_firewall_default_zone:
+  ensure  => present,
+  target  => lookup("lsst_firewall_default_target"),
+  sources => lookup("lsst_firewall_default_sources")
+  }
 
-	firewalld_service { 'Enable SSH':
-		ensure  => 'present',
-		service => 'ssh',
-	}
+  firewalld_service { 'Enable SSH':
+  ensure  => 'present',
+  service => 'ssh',
+  }
 
 # 	firewalld_service { 'Enable DHCP':
 # 		ensure  => 'present',
 # 		service => 'dhcpv6-client',
 # 	}
 
-# 	exec{"enable_icmp":
-# 		provider => "shell",
-# 		command => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",
-# 		require => Class["firewalld"],
-# 		onlyif => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
-# 	}
+  exec{"enable_icmp":
+    provider => "shell",
+    command  => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",
+    require  => Class["firewalld"],
+    onlyif   => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
+  }
 # 	Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 # 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
 # 'acpid', 'wget', 'nmap']:
