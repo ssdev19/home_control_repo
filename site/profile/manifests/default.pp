600516d6163cd7078c48b38b8ef42774e11c8e30
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2021-04-22 11:19:22 -0700

    add motd

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -1,7 +1,11 @@
 class profile::default {
 #  include profile::it::monitoring
 # All telegraf configuration came from Hiera
-
+  $motd_msg = lookup('motd')
+  file { '/etc/motd' :
+    ensure  => file,
+    content => $motd_msg,
+  }
 include ssh
 #include accounts
 
