036683efd0d68fa05fe52cf5618043ad3d7b2260
AnonSS <mysticguy9@yahoo.com>
2021-04-26 14:49:12 -0700

    az

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -32,7 +32,7 @@ class profile::core::common(
 #  if $collect_metrics {
 #    include profile::core::telegraf
 #  }
-Puppet_authorization::Rule <| |> ~> Service['pe-puppetserver']
+
   Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
 'acpid', 'wget', 'nmap']:
