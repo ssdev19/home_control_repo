a522b0e9b502531c41398ff133d99fbb475050e0
AnonSS <mysticguy9@yahoo.com>
2021-04-28 15:34:36 -0700

    az

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -1,7 +1,9 @@
 class profile::win {
-  include chocolatey
+#  include chocolatey
   include 'nsclient'
-
+class { 'nsclient':
+  allowed_hosts => ['10.21.0.0/22','10.21.4.0/22'],
+}
 file { 'c:\backups':
   ensure  => directory,
 }
