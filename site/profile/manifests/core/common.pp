388ff3bef37503a44461ddeab1bcebe1d82245e1
AnonSS <mysticguy9@yahoo.com>
2021-04-28 07:45:28 -0700

    z

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -35,9 +35,8 @@ class profile::core::common(
 
 file { '/root/secretfile.cfg':
   ensure  => file,
-  content => 'this string will be encrypted is your catalog'.node_encrypt::secret
+  content => 'this string will be encrypted in your catalog\r'.node_encrypt::secret
 }
-
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
 'acpid', 'wget', 'nmap']:
