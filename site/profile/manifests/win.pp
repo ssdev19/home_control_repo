ed683c120a0c886e8f7d517f13063b1846fe678c
AnonSS <mysticguy9@yahoo.com>
2021-04-28 15:47:05 -0700

    x

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -1,6 +1,6 @@
 class profile::win {
-#  include chocolatey
-  include 'nsclient'
+  include chocolatey
+#  include 'nsclient'
 class { 'nsclient':
   allowed_hosts => ['10.21.0.0/22','10.21.4.0/22'],
 }
