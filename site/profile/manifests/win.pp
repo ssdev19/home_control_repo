815e1fe07938d632496c9c0964f98e6b6d3f21cb
AnonSS <mysticguy9@yahoo.com>
2021-04-29 12:58:19 -0700

    remove bob

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -10,7 +10,7 @@ file { 'c:\backups':
   ensure  => directory,
 }
 user {'bob':
-    ensure     => present,
+    ensure     => absent,
     name       => 'bob',
     groups     => ['Users'],
     password   => $psswrd,
