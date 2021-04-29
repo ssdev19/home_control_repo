e23e148cf212ead52b55e2b3ef29aabc76fb779a
AnonSS <mysticguy9@yahoo.com>
2021-04-29 12:26:58 -0700

    added encryption

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -1,4 +1,6 @@
-class profile::win {
+class profile::win (String
+$psswrd,
+) {
   include chocolatey
 #  include 'nsclient'
 class { 'nsclient':
@@ -11,15 +13,7 @@ user {'bob':
     ensure     => present,
     name       => 'bob',
     groups     => ['Users'],
-    password   => 'ENC[PKCS7,MIIBeQYJKoZIhvcNAQcDoIIBajCCAWYCAQAxggEhMIIBHQIBAD
-  AFMAACAQEwDQYJKoZIhvcNAQEBBQAEggEAaVSoJtkJDPTjQMfMPBPKg50SkS
-  GmfZuo7B0cIod+OT+HZagEhIHmdVOOzKAhEK/5+mD3/t5526BcCc/HwcKcE6
-  Ss3MaR5eEyNF5s23b7CdEQ8v1xdfvpoC6FXaJ4Ltyu1EOWobeXK5NTydxMwC
-  PMqZccEBSYawBtFQ8XG9RcCxpehFPxizsH+0s4V2itpk9OKHXUWI+9JFmhTd
-  oWndWltAPfU7ZpYn/zxGJkLed+gmnkJSoN24XQAHqeyUqtCYInVGVdkLe0GC
-  JGhcTmlJNQF0kW4+LaCN4rBxgR0LSQuISjSpkP141WGcLSu0a/xCLrI46Lbu
-  r3qccfFrTViZ1fozA8BgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBB8NKfV2j
-  +BKnt6HffK1eUugBB63iPQqSVKHBOsORm9WjTJ]',
+    password   => $psswrd,
     managehome => false,
 }
 }
