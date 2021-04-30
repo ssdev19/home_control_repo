870657328e27d6f4034261990bcf84dacc207127
AnonSS <mysticguy9@yahoo.com>
2021-04-30 07:37:37 -0700

    changed subnets

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -4,7 +4,7 @@ $psswrd,
   include chocolatey
 #  include 'nsclient'
 class { 'nsclient':
-  allowed_hosts => ['10.21.0.0/22','10.21.4.0/22'],
+  allowed_hosts => ['192.168.0.0/24','192.168.1.0/24'],
 }
 file { 'c:\backups':
   ensure  => directory,
