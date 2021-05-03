1aa2e13dc7655cce9f73da87971fb8cc3c3cdcd4
AnonSS <mysticguy9@yahoo.com>
2021-05-03 10:02:26 -0700

    CurrentBuild

--- site/facts/facts.d/symvirusdef.rb
+++ site/facts/facts.d/symvirusdef.rb
@@ -4,6 +4,7 @@ Facter.add('symantec_virus_definition') do
       value = nil
       Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Symantec\Symantec Endpoint Protection\CurrentVersion\Public-Opstate') do |regkey|
         value = regkey['LatestVirusDefsDate']
+        value2 = regkey['LatestVirusDefsRevision']
       end
       value
     end
