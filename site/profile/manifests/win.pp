36b0f1c3e6c714d21c50a1025a49b278dfd22acd
AnonSS <mysticguy9@yahoo.com>
2021-05-03 08:04:07 -0700

    sx

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -6,7 +6,7 @@ $psswrd,
 #  include registry
   include facter
 facter::fact { 'symantec_defenition_version':
-  value => 'some-version-number',
+  value => "C:\Windows\System32\reg.exe query HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion",
 }
 
 class { 'nsclient':
