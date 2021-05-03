74a4a227def2f47d12dc6676c4a4263c4d9a1b79
AnonSS <mysticguy9@yahoo.com>
2021-05-03 09:13:01 -0700

    symantec_virus_definition

---
+++ site/facts/facts.d/symvirusdef.rb
@@ -0,0 +1,10 @@
+Facter.add('symantec_virus_definition') do
+    confine :osfamily => :windows
+    setcode do
+      value = nil
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Symantec\Symantec Endpoint Protection\CurrentVersion\Public-Opstate') do |regkey|
+        value = regkey['LatestVirusDefsDate']
+      end
+      value
+    end
+  end
