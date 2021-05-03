01c561bd1c8318ba4e18263d9cd522f45d1a34ac
AnonSS <mysticguy9@yahoo.com>
2021-05-03 08:55:49 -0700

    x

--- site/facts/facts.d/windowsedition.rb
+++ site/facts/facts.d/windowsedition.rb
@@ -2,7 +2,7 @@ Facter.add('windows_edition_custom') do
   confine :osfamily => :windows
   setcode do
     value = nil
-    Win32::Registry::.open('HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
       value = regkey['EditionID']
     end
     value
