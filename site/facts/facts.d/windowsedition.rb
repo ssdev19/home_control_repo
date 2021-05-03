08fdce39831d59d0ad1f3c8c72907a1be2e0aeb9
AnonSS <mysticguy9@yahoo.com>
2021-05-03 10:11:49 -0700

    az

--- site/facts/facts.d/windowsedition.rb
+++ site/facts/facts.d/windowsedition.rb
@@ -3,7 +3,8 @@ Facter.add('windows_edition_custom') do
   setcode do
     value = nil
     Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
-      value = regkey['EditionID']; ['CurrentBuild']
+      value = regkey['EditionID'] 
+      value = regkey['CurrentBuild']
     end
     value
   end
