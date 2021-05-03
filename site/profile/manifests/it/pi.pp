fe6807b339a41bd2cf97cc5f4522181fdedec9ca
AnonSS <mysticguy9@yahoo.com>
2021-05-03 16:23:48 -0700

    sx

--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -3,4 +3,7 @@ class profile::it::pi {
   include ssh
   include timezone
   include accounts
+service { 'puppet service':
+  ensure => running,
+  name   => 'puppet',
 }
--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -10,7 +10,7 @@ facter::fact { 'symantec_defenition_version':
 }
 
 class { 'nsclient':
-  allowed_hosts => ['192.168.0.0/24','192.168.1.0/22'],
+  allowed_hosts => ['192.168.0.0/24','192.168.1.0/24'],
 }
 file { 'c:\backups':
   ensure  => directory,
