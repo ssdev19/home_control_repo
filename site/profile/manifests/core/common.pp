bf92b0c96adfd2be66b6bc3dbe89a0860a70f06a
AnonSS <mysticguy9@yahoo.com>
2021-05-04 13:01:23 -0700

    include ssh

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -21,7 +21,7 @@ class profile::core::common(
   include accounts
   # include puppet_agent
   # include resolv_conf
-  # include ssh
+  include ssh
   # include easy_ipa
   # include augeas
   # include rsyslog
