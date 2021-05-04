724a032da586a97d091461df344414ecbffe773c
AnonSS <mysticguy9@yahoo.com>
2021-05-04 10:14:51 -0700

    sx

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -4,7 +4,7 @@ class profile::core::puppet_master {
 #   provider => 'puppetserver_gem',
 # }
 include node_encrypt::certificates
-Puppet_authorization::Rule <| |> ~> Service['puppetserver']
+Puppet_authorization::Rule <| |> ~> Service['puppetserver Service']
 
   file{ '/root/README':
     ensure  => file,
