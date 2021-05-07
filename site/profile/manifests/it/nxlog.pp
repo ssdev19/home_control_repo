9628cf478a1691a1538226e45fab21c73cd878f7
AnonSS <mysticguy9@yahoo.com>
2021-05-07 12:09:57 -0700

    d

--- site/profile/manifests/it/nxlog.pp
+++ site/profile/manifests/it/nxlog.pp
@@ -1,8 +1,8 @@
 class profile::it::nxlog {
   class {'nxlog':
   ensure_setting => latest,
-  nxlog_root     => 'C:\\Program Files (x86)\\nxlog',
-  conf_dir       => 'C:\\Program Files (x86)\\nxlog\\conf', # note the /'s here
+  nxlog_root     => 'C:\Program Files (x86)\nxlog',
+  conf_dir       => 'C:/Program Files (x86)/nxlog/conf', # note the /'s here
   conf_file      => 'nxlog.conf',
   }
 #   nxlog::config::extension { 'gelf':
