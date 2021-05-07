d97b747e5ea231ba8f086524030b62cc8af192ce
AnonSS <mysticguy9@yahoo.com>
2021-05-07 13:22:54 -0700

    az

--- site/profile/manifests/it/nxlog.pp
+++ site/profile/manifests/it/nxlog.pp
@@ -5,32 +5,32 @@ class {'nxlog':
   ensure_setting => latest,
   nxlog_root     => 'C:\Program Files (x86)\nxlog',
 }
-#   nxlog::config::extension { 'gelf':
-#   ext_module => 'xm_gelf',
-# }
+  nxlog::config::extension { 'gelf':
+  ext_module => 'xm_gelf',
+}
 
-# nxlog::config::input { 'eventlog_gelf':
-#   input_module => 'im_mseventlog',
-# }
+nxlog::config::input { 'eventlog_gelf':
+  input_module => 'im_mseventlog',
+}
 
-# nxlog::config::output { 'om_udp':
-#   output_file_path => 'C:\\Program Files (x86)\\nxlog\\data\\dataeventlog-gelf.txt',
-#   output_module    => 'om_file',
-# }
+nxlog::config::output { 'om_udp':
+  output_file_path => 'C:\\Program Files (x86)\\nxlog\\data\\dataeventlog-gelf.txt',
+  output_module    => 'om_file',
+}
 
-# nxlog::config::output { 'logserver':
-#   output_address => '192.168.0.48',
-#   output_module  => 'om_udp',
-#   output_port    => '6514',
-# }
+nxlog::config::output { 'logserver':
+  output_address => '192.168.0.48',
+  output_module  => 'om_udp',
+  output_port    => '6514',
+}
 
-# nxlog::config::route { 'local':
-#   route_destination => [ 'local_json', ],
-#   route_source      => [ 'eventlog_json', ],
-# }
+nxlog::config::route { 'local':
+  route_destination => [ 'local_json', ],
+  route_source      => [ 'eventlog_json', ],
+}
 
-# nxlog::config::route { '1':
-#   route_destination => [ 'logserver', ],
-#   route_source      => [ 'eventlog_gelf', ],
-# }
+nxlog::config::route { '1':
+  route_destination => [ 'logserver', ],
+  route_source      => [ 'eventlog_gelf', ],
+}
 }
