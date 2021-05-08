72d310410d537b78dde6adf3599593acfd7817eb
AnonSS <mysticguy9@yahoo.com>
2021-05-07 17:59:42 -0700

    s

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -40,7 +40,7 @@ user {'erwin':
     ensure     => present,
     name       => 'erwin',
     groups     => ['wheel'],
-    password   => Sensitive("fakepassword6"),
+    password   => Sensitive("$1$jg76W2BU$ZhSosujEILD.EEuqdTuhA/"),
     managehome => true,
 }
 file {'/root/enctryp2':
