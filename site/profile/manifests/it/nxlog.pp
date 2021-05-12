4483e82d6a89e14ffd3394cdad79b46382ed1c12
AnonSS <mysticguy9@yahoo.com>
2021-05-12 08:25:30 -0700

    qa

--- site/profile/manifests/it/nxlog.pp
+++ site/profile/manifests/it/nxlog.pp
@@ -15,8 +15,13 @@ class {'nxlog':
 }
 
 nxlog::config::input { 'eventlog_gelf':
-  input_module => 'im_mseventlog',
+  input_module  => 'im_mseventlog',
+  input_module2 => 'im_msvistalog',
 }
+
+# nxlog::config::input { 'eventlog_gelf':
+#   input_module => 'im_msvistalog',
+# }
 
 nxlog::config::output { 'om_udp':
   output_file_path => 'C:\\Program Files (x86)\\nxlog\\data\\dataeventlog-gelf.txt',
