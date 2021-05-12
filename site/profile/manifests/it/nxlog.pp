21e800624d1fb1e2ec69d47ad026bd048783cbea
AnonSS <mysticguy9@yahoo.com>
2021-05-12 11:13:45 -0700

    dc

--- site/profile/manifests/it/nxlog.pp
+++ site/profile/manifests/it/nxlog.pp
@@ -30,7 +30,7 @@ nxlog::config::output { 'om_udp':
 }
 
 nxlog::config::output { 'logserver':
-  output_address => unwrap($graylogip_hide).node_encrypt::secret,
+  output_address => unwrap($graylogip_hide),
   output_module  => 'om_udp',
   output_port    => '6514',
 }
