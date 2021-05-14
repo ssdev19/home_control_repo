e72a6ddaf4bb8fe0f9be376cb8df7f5ef7d2c7e5
AnonSS <mysticguy9@yahoo.com>
2021-05-14 15:16:24 -0700

    moved prometheus to yaml

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -1,47 +1,48 @@
 # Prometheus monitoring URL: http://prometheus.home.vm:9090/ 
 class profile::it::prometheus {
-class { 'prometheus::node_exporter':
-  version            => '0.12.0',
-  collectors_disable => ['loadavg', 'mdadm'],
-  extra_options      => '--collector.ntp.server ntp1.orange.intra',
-}
-class { 'prometheus::server':
-  version        => '2.26.0',
-  alerts         => {
-    'groups' => [
-      {
-        'name'  => 'alert.rules',
-        'rules' => [
-          {
-            'alert'       => 'InstanceDown',
-            'expr'        => 'up == 0',
-            'for'         => '5m',
-            'labels'      => {
-              'severity' => 'page',
-            },
-            'annotations' => {
-              'summary'     => 'Instance {{ $labels.instance }} down',
-              'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
-            }
-          },
-        ],
-      },
-    ],
-  },
-  scrape_configs => [
-    {
-      'job_name'        => 'prometheus',
-      'scrape_interval' => '10s',
-      'scrape_timeout'  => '10s',
-      'static_configs'  => [
-        {
-          'targets' => [ 'localhost:9090', 'grafana.home.vm:9100', '192.168.0.107:9182' ],
-          'labels'  => {
-            'alias' => 'Prometheus',
-          }
-        }
-      ],
-    },
-  ],
-}
+  include prometheus
+# class { 'prometheus::node_exporter':
+#   version            => '0.12.0',
+#   collectors_disable => ['loadavg', 'mdadm'],
+#   extra_options      => '--collector.ntp.server ntp1.orange.intra',
+# }
+# class { 'prometheus::server':
+#   version        => '2.26.0',
+#   alerts         => {
+#     'groups' => [
+#       {
+#         'name'  => 'alert.rules',
+#         'rules' => [
+#           {
+#             'alert'       => 'InstanceDown',
+#             'expr'        => 'up == 0',
+#             'for'         => '5m',
+#             'labels'      => {
+#               'severity' => 'page',
+#             },
+#             'annotations' => {
+#               'summary'     => 'Instance {{ $labels.instance }} down',
+#               'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
+#             }
+#           },
+#         ],
+#       },
+#     ],
+#   },
+#   scrape_configs => [
+#     {
+#       'job_name'        => 'prometheus',
+#       'scrape_interval' => '10s',
+#       'scrape_timeout'  => '10s',
+#       'static_configs'  => [
+#         {
+#           'targets' => [ 'localhost:9090', 'grafana.home.vm:9100', '192.168.0.107:9182' ],
+#           'labels'  => {
+#             'alias' => 'Prometheus',
+#           }
+#         }
+#       ],
+#     },
+#   ],
+# }
 }
