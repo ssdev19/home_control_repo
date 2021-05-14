f61763e9c0a4dae4bdfdedc6b60a2d86875a3644
AnonSS <mysticguy9@yahoo.com>
2021-05-14 12:10:37 -0700

    a

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -35,7 +35,7 @@ class { 'prometheus::server':
       'scrape_timeout'  => '10s',
       'static_configs'  => [
         {
-          'targets' => [ 'localhost:9090', '192.168.0.107:9182', 'grafana.home.vm:9090' ],
+          'targets' => [ 'localhost:9090', '192.168.0.107:9182' ],
           'labels'  => {
             'alias' => 'Prometheus',
           }
