66b4aaebe6c8bae4cf701a82e388a25fd7a4ebba
AnonSS <mysticguy9@yahoo.com>
2021-05-14 09:58:35 -0700

    az

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -1,6 +1,18 @@
 class profile::it::grafana {
   class { 'grafana':
-    version  => '7.5.3'
+    version                  => '7.5.3',
+    provisioning_datasources => {
+    apiVersion  => 1,
+    datasources => [
+      {
+        name      => 'Prometheus',
+        type      => 'prometheus',
+        access    => 'proxy',
+        url       => 'http://localhost:9090/prometheus',
+        isDefault => true,
+      },
+    ],
+  }
   }
 
   firewalld_port { 'Grafana Main Port':
