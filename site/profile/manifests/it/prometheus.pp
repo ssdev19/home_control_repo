a2cec657524b3716fa52c448c2ee2da914568a29
AnonSS <mysticguy9@yahoo.com>
2021-05-15 10:15:23 -0700

    az

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -1,49 +1,51 @@
 # Prometheus monitoring URL: http://prometheus.home.vm:9090/ 
-class profile::it::prometheus {
-  # include node_exporter
+class profile::it::prometheus (String
+$target
+) {
+  include node_exporter
 
-# class { 'prometheus::node_exporter':
-#   version            => '0.12.0',
-#   collectors_disable => ['loadavg', 'mdadm'],
-#   extra_options      => '--collector.ntp.server ntp1.orange.intra',
-# }
-# class { 'prometheus::server':
-#   version        => '2.26.0',
-#   alerts         => {
-#     'groups' => [
-#       {
-#         'name'  => 'alert.rules',
-#         'rules' => [
-#           {
-#             'alert'       => 'InstanceDown',
-#             'expr'        => 'up == 0',
-#             'for'         => '5m',
-#             'labels'      => {
-#               'severity' => 'page',
-#             },
-#             'annotations' => {
-#               'summary'     => 'Instance {{ $labels.instance }} down',
-#               'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
-#             }
-#           },
-#         ],
-#       },
-#     ],
-#   },
-#   scrape_configs => [
-#     {
-#       'job_name'        => 'prometheus',
-#       'scrape_interval' => '10s',
-#       'scrape_timeout'  => '10s',
-#       'static_configs'  => [
-#         {
-#           'targets' => [ 'localhost:9090', 'grafana.home.vm:9100', '192.168.0.107:9182' ],
-#           'labels'  => {
-#             'alias' => 'Prometheus',
-#           }
-#         }
-#       ],
-#     },
-#   ],
-# }
+class { 'prometheus::node_exporter':
+  version            => '0.12.0',
+  collectors_disable => ['loadavg', 'mdadm'],
+  extra_options      => '--collector.ntp.server ntp1.orange.intra',
+}
+class { 'prometheus::server':
+  version        => '2.26.0',
+  alerts         => {
+    'groups' => [
+      {
+        'name'  => 'alert.rules',
+        'rules' => [
+          {
+            'alert'       => 'InstanceDown',
+            'expr'        => 'up == 0',
+            'for'         => '5m',
+            'labels'      => {
+              'severity' => 'page',
+            },
+            'annotations' => {
+              'summary'     => 'Instance {{ $labels.instance }} down',
+              'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
+            }
+          },
+        ],
+      },
+    ],
+  },
+  scrape_configs => [
+    {
+      'job_name'        => 'prometheus',
+      'scrape_interval' => '10s',
+      'scrape_timeout'  => '10s',
+      'static_configs'  => [
+        {
+          'targets' => $target,
+          'labels'  => {
+            'alias' => 'Prometheus',
+          }
+        }
+      ],
+    },
+  ],
+}
 }
