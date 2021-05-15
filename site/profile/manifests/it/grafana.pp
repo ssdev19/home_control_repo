b6272dc8ef715db29335f9c76a94093331669823
AnonSS <mysticguy9@yahoo.com>
2021-05-15 08:19:54 -0700

    x

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -1,10 +1,5 @@
 # Grafana url: http://192.168.0.46:3000
 class profile::it::grafana {
-  class { 'prometheus::node_exporter':
-  version            => '0.12.0',
-  collectors_disable => ['loadavg', 'mdadm'],
-  extra_options      => '--collector.ntp.server ntp1.orange.intra',
-  }
   class { 'grafana':
     version                  => '7.5.3',
     provisioning_datasources => {
