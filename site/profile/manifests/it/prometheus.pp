594c75958dcbfc271a215ff4cd581d642f3c99b8
AnonSS <mysticguy9@yahoo.com>
2021-05-15 10:28:58 -0700

    s

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -1,7 +1,7 @@
 # Prometheus monitoring URL: http://prometheus.home.vm:9090/ 
 class profile::it::prometheus {
   include node_exporter
-$target = $"${fqdn}:9100"
+$target = "${::fqdn}:9100"
 class { 'prometheus::node_exporter':
   version            => '0.12.0',
   collectors_disable => ['loadavg', 'mdadm'],
