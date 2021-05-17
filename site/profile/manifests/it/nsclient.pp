30ad3ee1c0306f2b4abb8e3bbad2534cd742a85c
AnonSS <mysticguy9@yahoo.com>
2021-05-17 11:43:58 -0700

    nsclient

---
+++ site/profile/manifests/it/nsclient.pp
@@ -0,0 +1,10 @@
+# nscliet
+class profile::it::grafana {
+class { 'nsclient':
+  package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7/NSClient.-0.3.7-x64.msi',
+  package_name            => 'NSClient++ (Win64)',
+  package_source          => '0.3.7.msi',
+  allowed_hosts           => ['140.252.32.34'],
+  password                => test_pwd,
+  service_enable          => true,
+}
