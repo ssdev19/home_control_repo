185bfe854e184f8ead0ba6866c0fda3b440f7e0e
AnonSS <mysticguy9@yahoo.com>
2021-05-17 13:53:05 -0700

    az

--- site/profile/manifests/it/nsclient.pp
+++ site/profile/manifests/it/nsclient.pp
@@ -1,11 +1,13 @@
 # nscliet
-class profile::it::nsclient {
+class profile::it::nsclient (Sensitive[String]
+$nagios_password
+){
   class { 'nsclient':
     package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7',
     package_name            => 'NSClient++ (x64)',
     package_source          => 'NSClient.-0.3.7-x64.msi',
     allowed_hosts           => ['140.252.32.34'],
-    password                => testpwd,
+    password                => $nagios_password,
     service_enable          => true,
   }
   service { 'NSClientpp':
