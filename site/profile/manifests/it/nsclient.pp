a0d2ca3640402c2b1a6a6fe7063fb37b29b59c94
AnonSS <mysticguy9@yahoo.com>
2021-05-17 12:17:20 -0700

    0.3.7-x64.msi

--- site/profile/manifests/it/nsclient.pp
+++ site/profile/manifests/it/nsclient.pp
@@ -3,7 +3,7 @@ class profile::it::nsclient {
   class { 'nsclient':
     package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7/NSClient.-0.3.7-x64.msi',
     package_name            => 'NSClient++ (x64)',
-    package_source          => '0.3.7.msi',
+    package_source          => '0.3.7-x64.msi',
     allowed_hosts           => ['140.252.32.34'],
     password                => testpwd,
     service_enable          => true,
