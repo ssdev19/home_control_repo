f177d5fa0974be694ac5ee925c010ff8434c07a6
AnonSS <mysticguy9@yahoo.com>
2021-05-17 12:28:38 -0700

    x

--- site/profile/manifests/it/nsclient.pp
+++ site/profile/manifests/it/nsclient.pp
@@ -1,7 +1,7 @@
 # nscliet
 class profile::it::nsclient {
   class { 'nsclient':
-    package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7/',
+    package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7',
     package_name            => 'NSClient++ (x64)',
     package_source          => '0.3.7.msi',
     allowed_hosts           => ['140.252.32.34'],
