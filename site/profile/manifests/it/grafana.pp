2df83954c1b01e29f5cf314935d72943079cc4d9
AnonSS <mysticguy9@yahoo.com>
2021-05-21 12:17:23 -0700

    sx

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -19,7 +19,7 @@ $ldaphost_hide,
       },
     ],
   'auth.ldap'   => {
-    enabled     => 'true',
+    enabled     => 'false',
     config_file => '/etc/grafana/ldap.toml',
   },
       ldap_cfg  => {
