8922edab5bc59d864ee2b6326b9d51564b48d2ef
AnonSS <mysticguy9@yahoo.com>
2021-05-24 08:24:13 -0700

    sx

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -41,9 +41,10 @@ file {'/root/enctryp2':
     owner   => 'root',
     content => unwrap($psswrd_encrypt).node_encrypt::secret,
   }
-file_line { 'Append a 5nd line to /root/enctryp2':
+file_line { 'Append a 6nd line to /root/enctryp2':
+  ensure => present
   path => '/root/enctryp2',
-  line => 'Want to add this 5nd line as a test',
+  line => 'Want to add this 6nd line as a test',
   match => 'Want to add this 5nd',
   match_for_absence => true,
   multiple => true,
