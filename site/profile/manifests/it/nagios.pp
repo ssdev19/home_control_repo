60c0aeab911f8fba8d79d27207821a4810d2f852
AnonSS <mysticguy9@yahoo.com>
2021-05-24 12:03:40 -0700

    az

--- site/profile/manifests/it/nagios.pp
+++ site/profile/manifests/it/nagios.pp
@@ -1,17 +1,18 @@
 # nagios
 class profile::it::nagios {
+  include ::nrpe
   # Install Nagios server
-  class { 'nagios':
-    nrpe        => true,                     # Set up NRPE for monitoring of remote hosts
-    nsca        => false,                    # Skip NSCA, which is needed for passive checks
-    selinux     => true,                     # Manage SELinux policies to allow Nagios to run smoothly
-    firewall    => true,                     # Manage firewall rules to allow Nagios/NRPE to run smoothly
-    url         => 'nagios.home.vm',     # Service URL of Nagios, if different from the system hostname
-    serveradmin => 'root@home.vm',       # Admin's email address
-    ssl_cert    => '/etc/pki/tls/certs/nagios.home.vm.pem',  # Path to SSL cert for HTTPS
-    ssl_key     => '/etc/pki/tls/private/nagios.home.vm.key',  # Path to SSL key for HTTPS
-    auth_type   => 'CAS',                    # Override Apache basic auth and use CAS single sign-on instead
-  }
+  # class { 'nagios':
+  #   nrpe        => true,                     # Set up NRPE for monitoring of remote hosts
+  #   nsca        => false,                    # Skip NSCA, which is needed for passive checks
+  #   selinux     => true,                     # Manage SELinux policies to allow Nagios to run smoothly
+  #   firewall    => true,                     # Manage firewall rules to allow Nagios/NRPE to run smoothly
+  #   url         => 'nagios.home.vm',     # Service URL of Nagios, if different from the system hostname
+  #   serveradmin => 'root@home.vm',       # Admin's email address
+  #   ssl_cert    => '/etc/pki/tls/certs/nagios.home.vm.pem',  # Path to SSL cert for HTTPS
+  #   ssl_key     => '/etc/pki/tls/private/nagios.home.vm.key',  # Path to SSL key for HTTPS
+  #   auth_type   => 'CAS',                    # Override Apache basic auth and use CAS single sign-on instead
+  # }
 
   # Deploy HTTPS certificate
   # file { '/etc/pki/tls/certs/nagios.example.com.pem':
--- site/profile/manifests/it/nsclient.pp
+++ site/profile/manifests/it/nsclient.pp
@@ -2,5 +2,16 @@
 class profile::it::nsclient (Sensitive[String]
 $nagios_password,
 ){
-include ::nrpe
+  class { 'nsclient':
+    package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7',
+    package_name            => 'NSClient++ (x64)',
+    package_source          => 'NSClient.-0.3.7-x64.msi',
+    allowed_hosts           => ['140.252.32.34'],
+    password                => unwrap($nagios_password),
+    service_enable          => true,
+  }
+  service { 'NSClientpp':
+    ensure => 'running',
+    enable => true,
+}
 }
