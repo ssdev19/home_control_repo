9499ffa240064b9425f1364f12f54b4fe421c5b5
AnonSS <mysticguy9@yahoo.com>
2021-05-24 10:05:07 -0700

    xc

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -42,7 +42,7 @@ file {'/root/enctryp2':
     content => unwrap($psswrd_encrypt).node_encrypt::secret,
   }
 augeas { "vaugeasfile title":
-  context => "/root/vaugeasfile/",
+  context => "/root/vaugeasfile/*/",
   # Only if no node exists for http_proxy
   # onlyif  => "match password/var[. = 'fakepwd'] size==0",
   changes => [
