e28473ad3cc28c2562f0677248b9e4a97d9c577e
AnonSS <mysticguy9@yahoo.com>
2021-05-24 14:12:57 -0700

    m

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -54,7 +54,7 @@ file {
   ;
   }
 ini_setting { "testfilecomment":
-  path => "c:\\backups\\nagiostest.cfg",
+  path => "c:\\backups\\nagiostest.txt",
   section => 'test',
   setting => 'cfg_file',
   value => "/usr/local/nagios/etc/objects/-${$nagioscfg}.cfg"
