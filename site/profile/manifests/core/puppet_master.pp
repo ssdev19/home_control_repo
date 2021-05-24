4c744684e8430189f68a1d2520d74d2c3233eb9c
AnonSS <mysticguy9@yahoo.com>
2021-05-24 10:03:15 -0700

    d

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -41,9 +41,14 @@ file {'/root/enctryp2':
     owner   => 'root',
     content => unwrap($psswrd_encrypt).node_encrypt::secret,
   }
-augeas::lens { 'augeasfile':
-  lens_content => file('/root/augeasfile'),
-  # test_content => file('networkmanager/lenses/test_networkmanager.aug'),
-  stock_since  => '1.0.0',
+augeas { "vaugeasfile title":
+  context => "/root/vaugeasfile/",
+  # Only if no node exists for http_proxy
+  # onlyif  => "match password/var[. = 'fakepwd'] size==0",
+  changes => [
+    "set ipaddr 192.168.100.3.",
+    # "set canonical test.localdomain",
+    # "set alias[1] test",
+  ],
 }
 }
