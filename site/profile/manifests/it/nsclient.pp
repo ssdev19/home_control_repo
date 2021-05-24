fc19819317c902fefabbb56119a4a2527b6ef50e
AnonSS <mysticguy9@yahoo.com>
2021-05-24 11:59:14 -0700

    mod 'link0-nrpe', '0.0.3'

--- site/profile/manifests/it/nsclient.pp
+++ site/profile/manifests/it/nsclient.pp
@@ -2,16 +2,5 @@
 class profile::it::nsclient (Sensitive[String]
 $nagios_password,
 ){
-  class { 'nsclient':
-    package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7',
-    package_name            => 'NSClient++ (x64)',
-    package_source          => 'NSClient.-0.3.7-x64.msi',
-    allowed_hosts           => ['140.252.32.34'],
-    password                => unwrap($nagios_password),
-    service_enable          => true,
-  }
-  service { 'NSClientpp':
-    ensure => 'running',
-    enable => true,
-}
+include ::nrpe
 }
