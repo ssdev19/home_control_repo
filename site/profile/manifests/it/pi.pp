1e3f21391bb5004455cf7fe51d4aa6e48333754e
AnonSS <mysticguy9@yahoo.com>
2021-05-25 11:14:43 -0700

    s

--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -3,7 +3,10 @@ class profile::it::pi {
   include ssh
   include timezone
   include accounts
-  include prometheus::node_exporter
+  # include prometheus::node_exporter
+  class {'::puppet_agent':
+  package_version => '6.21.1',
+  }
 # class { 'prometheus::node_exporter':
 #   version            => '0.16.2',
 #   collectors_disable => ['loadavg', 'mdadm'],
