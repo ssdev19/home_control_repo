29a9bfb0fe4b635a7f45f74a34cfe1b6567a937b
AnonSS <mysticguy9@yahoo.com>
2021-05-27 08:00:42 -0700

    s

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -7,7 +7,7 @@ $ldaphost_hide,
 class { 'prometheus::node_exporter':
   version            => '0.16.0',
   collectors_disable => ['loadavg', 'mdadm'],
-  extra_options      => '--collector.ntp.server ntp1.orange.intra',
+  # extra_options      => '--collector.ntp.server ntp1.orange.intra',
 }
   class { 'grafana':
     version                  => '7.5.3',
--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -7,11 +7,7 @@ class profile::it::pi {
   # class {'::puppet_agent':
   # package_version => '6.21.1',
   # }
-class { 'prometheus::node_exporter':
-  version            => '0.15.0',
-  collectors_disable => ['loadavg', 'mdadm'],
-  extra_options      => '--collector.ntp.server ntp1.orange.intra',
-}
+
 # service { 'Puppet agent':
 #   ensure   => running,
 #   provider => init,
