103873cd85c98cc65ed1eb04e54a3a34e11135f1
AnonSS <mysticguy9@yahoo.com>
2021-05-27 07:54:45 -0700

    z

--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -7,11 +7,11 @@ class profile::it::pi {
   # class {'::puppet_agent':
   # package_version => '6.21.1',
   # }
-# class { 'prometheus::node_exporter':
-#   version            => '0.16.2',
-#   collectors_disable => ['loadavg', 'mdadm'],
-#   extra_options      => '--collector.ntp.server ntp1.orange.intra',
-# }
+class { 'prometheus::node_exporter':
+  version            => '0.16.0',
+  collectors_disable => ['loadavg', 'mdadm'],
+  extra_options      => '--collector.ntp.server ntp1.orange.intra',
+}
 # service { 'Puppet agent':
 #   ensure   => running,
 #   provider => init,
