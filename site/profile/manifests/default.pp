47b0272e4e62977eb86ef10808e5e19ce3a689e7
AnonSS <mysticguy9@yahoo.com>
2021-05-28 14:40:59 -0700

    z

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -5,7 +5,7 @@ class profile::default {
   include ssh
   include timezone
   include accounts
-  include blackbox_exporter
+  # include node_exporter
 
   #   $fqdn = $::fqdn
   # profile::it::prometheus::target: { "${fqdn} - node_exporter":
