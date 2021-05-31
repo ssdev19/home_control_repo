814d7869061bafb17172cccf774e41d9dba1c4cc
AnonSS <mysticguy9@yahoo.com>
2021-05-31 15:10:49 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -3,49 +3,10 @@ class profile::it::prometheus {
   # include node_exporter
   include prometheus
 
-# $target = "${::fqdn}:9100"
-# # class { 'prometheus::node_exporter':
-# #   version            => '0.12.0',
-# #   collectors_disable => ['loadavg', 'mdadm'],
-# #   extra_options      => '--collector.ntp.server ntp1.orange.intra',
-# # }
-# class { 'prometheus::server':
-#   version        => '2.27.0',
-#   alerts         => {
-#     'groups' => [
-#       {
-#         'name'  => 'alert.rules',
-#         'rules' => [
-#           {
-#             'alert'       => 'InstanceDown',
-#             'expr'        => 'up == 0',
-#             'for'         => '5m',
-#             'labels'      => {
-#               'severity' => 'page',
-#             },
-#             'annotations' => {
-#               'summary'     => 'Instance {{ $labels.instance }} down',
-#               'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
-#             }
-#           },
-#         ],
-#       },
-#     ],
-#   },
-#   scrape_configs => [
-#     {
-#       'job_name'        => 'prometheus',
-#       'scrape_interval' => '10s',
-#       'scrape_timeout'  => '10s',
-#       'static_configs'  => [
-#         {
-#           'targets' => $target,
-#           'labels'  => {
-#             'alias' => 'Prometheus',
-#           }
-#         }
-#       ],
-#     },
-#   ],
-# }
+user {'blackbox_exporter':
+    ensure     => present,
+    name       => 'blackbox_exporter',
+    groups     => ['blackbox_exporter'],
+    managehome => false,
+}
 }
