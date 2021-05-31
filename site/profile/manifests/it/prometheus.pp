722185bf87227123e688a6db3f5a0633df37f01b
AnonSS <mysticguy9@yahoo.com>
2021-05-31 15:38:46 -0700

    dc

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -13,7 +13,8 @@ class { 'archive':
   archives => { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64.tar.gz' => {
     'ensure' => 'present',
     'source' => 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.19.0/blackbox_exporter-0.19.0.linux-amd64.tar.gz',
-    extract  => true,
+    'extract'  => true,
+    'extract_path'  => '/usr/tmp',
 }, }
 }
 }
