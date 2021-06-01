98b5bf35ee5bfc8923d26b34aea6b0c113f2a00f
AnonSS <mysticguy9@yahoo.com>
2021-06-01 08:58:16 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -8,19 +8,19 @@ class profile::it::prometheus {
   # collectors_disable => ['loadavg', 'mdadm'],
   # extra_options      => '--collector.ntp.server ntp1.orange.intra',
 }
-user {'blackbox_exporter':
-    ensure     => present,
-    name       => 'blackbox_exporter',
-    groups     => ['blackbox_exporter'],
-    managehome => false,
-}
-archive { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64.tar.gz':
-    ensure       => 'present',
-    source       => 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.19.0/blackbox_exporter-0.19.0.linux-amd64.tar.gz',
-    extract      => true,
-    extract_path => '/usr/tmp',
-    cleanup      => true,
-}
+# user {'blackbox_exporter':
+#     ensure     => present,
+#     name       => 'blackbox_exporter',
+#     groups     => ['blackbox_exporter'],
+#     managehome => false,
+# }
+# archive { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64.tar.gz':
+#     ensure       => 'present',
+#     source       => 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.19.0/blackbox_exporter-0.19.0.linux-amd64.tar.gz',
+#     extract      => true,
+#     extract_path => '/usr/tmp',
+#     cleanup      => true,
+# }
 # file { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter' :
 #   ensure => present,
 #   target => '/root/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter',
