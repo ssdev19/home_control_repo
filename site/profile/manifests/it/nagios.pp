00a2406084b1a6111c2494e69fb3ac0df274a436
AnonSS <mysticguy9@yahoo.com>
2021-06-01 06:43:00 -0700

    w

--- site/profile/manifests/it/nagios.pp
+++ site/profile/manifests/it/nagios.pp
@@ -1,6 +1,24 @@
 # nagios
 class profile::it::nagios {
   include ::nrpe
+
+user {'blackbox_exporter':
+    ensure     => present,
+    name       => 'blackbox_exporter',
+    groups     => ['blackbox_exporter'],
+    managehome => false,
+}
+archive { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64.tar.gz':
+    ensure       => 'present',
+    source       => 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.19.0/blackbox_exporter-0.19.0.linux-amd64.tar.gz',
+    extract      => true,
+    extract_path => '/usr/tmp',
+    cleanup      => true,
+}
+# file { '/usr/tmp/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter' :
+#   ensure => present,
+#   target => '/root/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter',
+# }
   # Install Nagios server
   # class { 'nagios':
   #   nrpe        => true,                     # Set up NRPE for monitoring of remote hosts
