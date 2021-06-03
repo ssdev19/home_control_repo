e94df03862f56f4ce6e2266ce15e82e3b382daec
AnonSS <mysticguy9@yahoo.com>
2021-06-03 10:30:16 -0700

    az

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -18,7 +18,7 @@ $content,
       }
     }
   }
-$gmail_auth_token = lookup("gmail_auth_token")
+$gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
   version   => '0.22.2',
