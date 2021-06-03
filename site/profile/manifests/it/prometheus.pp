37862710f3dfea010da00cd2b67254c00cb8cf09
AnonSS <mysticguy9@yahoo.com>
2021-06-03 16:15:28 -0700

    az

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -43,7 +43,7 @@ class { 'prometheus::alertmanager':
       'name'          => 'email',
       'email_configs' => [
         {
-          'to'            => 'wf@belldex.com',
+          'to'            => 'shahram@lsst.org',
           'from'          => $gmail_account,
           'smarthost'     => 'smtp.gmail.com:587',
           'auth_username' => $gmail_account,
