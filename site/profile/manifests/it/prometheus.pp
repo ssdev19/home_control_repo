ca299628c56fd8729cbb60904019da045a70e71d
AnonSS <mysticguy9@yahoo.com>
2021-06-03 11:14:09 -0700

    s

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -24,7 +24,7 @@ class { 'prometheus::alertmanager':
   version   => '0.22.2',
   global    => {
     'resolve_timeout' => '1m',
-    }
+    },
   route     => {
     'group_by'        => ['alertname', 'cluster', 'service'],
     'group_wait'      => '30s',
