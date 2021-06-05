bfa7b55be332e4a85b498c6e3011492cdb12d7e1
AnonSS <mysticguy9@yahoo.com>
2021-06-05 11:53:32 -0700

    as

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -21,7 +21,7 @@ $content,
 $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
-  version   => '0.22.1',
+  version   => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
   #   'to'              => 'wf@belldex.com',
