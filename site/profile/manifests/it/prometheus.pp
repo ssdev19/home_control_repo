29c9012c44e7af58f4e32e6bc9f235499b95f83d
AnonSS <mysticguy9@yahoo.com>
2021-06-07 09:39:59 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -55,6 +55,7 @@ class { 'prometheus::alertmanager':
           'require_tls'   => true,
           'send_resolved' => true,
         },
+    {
       'name'          => 'slack',
       'slack_configs' => [
         {
@@ -62,8 +63,8 @@ class { 'prometheus::alertmanager':
           'channel'       => '#monitoring',
           'send_resolved' => true,
           'username'      => unwrap($slackuser_hide)
-        },
-      ],
+        },      ],
+    }
       ],
     },
   ],
