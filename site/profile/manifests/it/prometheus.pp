f16f3df4bbaa8b9caac451c2d6c7e3a18721f720
AnonSS <mysticguy9@yahoo.com>
2021-06-07 14:36:54 -0700

    az

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -66,18 +66,18 @@ class { 'prometheus::alertmanager':
           'username'      => unwrap($slackuser_hide)
         },
       ],
-      'email_configs' => [
-        {
-          'to'            => $gmail_account,
-          'from'          => $gmail_account,
-          'smarthost'     => 'smtp.gmail.com:587',
-          'auth_username' => $gmail_account,
-          'auth_identity' => $gmail_account,
-          'auth_password' => $gmail_auth_token,
-          'require_tls'   => true,
-          'send_resolved' => true,
-        },
-      ],
+      # 'email_configs' => [
+      #   {
+      #     'to'            => $gmail_account,
+      #     'from'          => $gmail_account,
+      #     'smarthost'     => 'smtp.gmail.com:587',
+      #     'auth_username' => $gmail_account,
+      #     'auth_identity' => $gmail_account,
+      #     'auth_password' => $gmail_auth_token,
+      #     'require_tls'   => true,
+      #     'send_resolved' => true,
+      #   },
+      # ],
     },
   ],
 }
