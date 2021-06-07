66a75b8b798e55c4348ca5a70e727cf5962e42c7
AnonSS <mysticguy9@yahoo.com>
2021-06-07 09:47:23 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -2,7 +2,6 @@
 class profile::it::prometheus (
 $content,
 $slackuser_hide,
-$slackapi_hide,
 ) {
   # include node_exporter
   include prometheus
@@ -23,8 +22,8 @@ $slackapi_hide,
 $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
-  extra_options       => '--cluster.listen-address=',
-  version             => '0.22.2',
+  extra_options => '--cluster.listen-address=',
+  version   => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
   #   'to'              => 'wf@belldex.com',
@@ -34,14 +33,14 @@ class { 'prometheus::alertmanager':
   #   'auth_identity'   => $gmail_account,
   #   'auth_password'   => $gmail_auth_token,
   #   },
-  route               => {
+  route     => {
     'group_by'        => ['alertname', 'job'],
     'group_wait'      => '30s',
     'group_interval'  => '5m',
     'repeat_interval' => '3h',
-    'receiver'        => ['email', 'slack'],
+    'receiver'        => 'email',
   },
-  receivers           => [
+  receivers => [
     {
       'name'          => 'email',
       'email_configs' => [
@@ -55,19 +54,8 @@ class { 'prometheus::alertmanager':
           'require_tls'   => true,
           'send_resolved' => true,
         },
-        ],
-    {
-      'name'          => 'slack',
-      'slack_configs' => [
-        {
-          'api_url'       => unwrap($slackapi_hide),
-          'channel'       => '#monitoring',
-          'send_resolved' => true,
-          'username'      => unwrap($slackuser_hide)
-        },
-    
       ],
-
-
+    },
+  ],
 }
-
+}
