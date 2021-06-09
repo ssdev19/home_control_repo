6390c22c43830f93d2364365129ea7900e908272
AnonSS <mysticguy9@yahoo.com>
2021-06-09 13:14:27 -0700

    az

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -10,7 +10,7 @@ class profile::core::common (
 ){
   include timezone
   include node_encrypt::certificates
-    include network
+  include network
   # include tuned
   # include chrony
   # include selinux
