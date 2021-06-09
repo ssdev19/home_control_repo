12963338d1b16ad5e57fa3212c629ddf6bd71d87
AnonSS <mysticguy9@yahoo.com>
2021-06-09 09:26:21 -0700

    ss

---
+++ site/profile/manifests/it/network.pp
@@ -0,0 +1,16 @@
+# Network configuration
+class profile::it::network (Sensitive[String]
+$ip_hide,
+$mask_hide,
+){
+  network_config { 'eth0':
+  ensure    => 'present',
+  family    => 'inet',
+  ipaddress => unwrap($ip_hide),
+  method    => 'static',
+  netmask   => unwrap($mask_hide),
+  onboot    => 'true',
+  hotplug   => 'true',
+  options   => {'pre-up' => 'sleep 2'},
+}
+}
