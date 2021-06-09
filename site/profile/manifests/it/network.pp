5f971bd9626ab831230c7ec69e24bd5ed8644f6f
AnonSS <mysticguy9@yahoo.com>
2021-06-09 11:09:31 -0700

    az

--- site/profile/manifests/it/network.pp
+++ site/profile/manifests/it/network.pp
@@ -3,15 +3,4 @@ class profile::it::network (Sensitive[String]
 $ip_hide,
 $mask_hide,
 ){
-include '::network'
-  network_config { 'eth0':
-  ensure    => 'present',
-  family    => 'inet',
-  ipaddress => unwrap($ip_hide),
-  method    => 'static',
-  netmask   => unwrap($mask_hide),
-  onboot    => 'true',
-  hotplug   => 'true',
-  options   => {'pre-up' => 'sleep 2'},
-}
 }
--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -19,7 +19,7 @@ class profile::it::pi {
 #   enable   => true,
 # }
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
+'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
 'acpid', 'wget', 'nmap', 'ifupdown-extra']:
 ensure => installed,
 }
