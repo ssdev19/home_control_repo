725164cded37c9bb0accd7b9d8486413fd149880
AnonSS <mysticguy9@yahoo.com>
2021-06-10 08:29:51 -0700

    aa

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -22,7 +22,8 @@ $slackuser_hide,
 $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
-  extra_options => '--cluster.listen-address=',
+  # extra_options => '--cluster.listen-address=',
+  extra_options => "--cluster.peer=%{facts.fqdn}:9094",
   version       => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
