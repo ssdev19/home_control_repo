475ea9c644d9cd10f29c0670fc3462ffd08ada81
AnonSS <mysticguy9@yahoo.com>
2021-06-11 09:41:54 -0700

    profile::it::prometheus::server_ip: "%{serverip}"

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -3,6 +3,7 @@ class profile::it::prometheus (Sensitive[String]
 $slackapi_hide,
 $slackuser_hide,
 $cluster_hide,
+$server_ip,
 ) {
   # include prometheus::node_exporter
   include prometheus
@@ -24,7 +25,7 @@ $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
   # extra_options => '--cluster.listen-address=',
-  extra_options => "--cluster.advertise-address=${serverip}:9093 \--cluster.peer=${unwrap($cluster_hide)}",
+  extra_options => "--cluster.advertise-address=${server_ip}:9093 \--cluster.peer=${unwrap($cluster_hide)}",
   version       => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
