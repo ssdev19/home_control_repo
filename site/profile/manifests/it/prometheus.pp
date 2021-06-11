adade5299d88866e97944ad8098f4003360ac660
AnonSS <mysticguy9@yahoo.com>
2021-06-11 14:06:34 -0700

    $advertise_ip

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -4,6 +4,7 @@ $slackapi_hide,
 $slackuser_hide,
 $cluster_hide,
 $server_ip,
+$advertise_ip,
 ) {
   # include prometheus::node_exporter
   include prometheus
@@ -25,7 +26,7 @@ $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
   # extra_options => '--cluster.listen-address=',
-  extra_options => "--cluster.advertise-address=127.0.0.1:9093 \--cluster.listen-address=:9797 \--cluster.peer=${unwrap($cluster_hide)}",
+  extra_options => "--cluster.advertise-address=${advertise_ip} \--cluster.listen-address=:9797 \--cluster.peer=${unwrap($cluster_hide)}",
   version       => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
