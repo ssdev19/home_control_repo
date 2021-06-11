4427b9ee3fdcc900427f4ffa9d4de3945bda68c1
AnonSS <mysticguy9@yahoo.com>
2021-06-11 08:15:36 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -24,7 +24,7 @@ $gmail_auth_token = lookup('gmail_auth_token')
 $gmail_account = lookup('gmail_account')
 class { 'prometheus::alertmanager':
   # extra_options => '--cluster.listen-address=',
-  extra_options => "--cluster.listen-address=:9094 \--cluster.peer=${unwrap($cluster_hide)}",
+  extra_options => "--cluster.listen-address=0.0.0.0:9094 \--cluster.peer=${unwrap($cluster_hide)}",
   version       => '0.22.2',
   # global    => {
   #   'resolve_timeout' => '1m',
