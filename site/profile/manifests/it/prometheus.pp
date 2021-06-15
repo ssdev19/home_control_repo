75008638f1147319c696cc7e90f1b2d9c97004fe
AnonSS <mysticguy9@yahoo.com>
2021-06-15 10:49:04 -0700

    dc

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -40,12 +40,19 @@ class { 'prometheus::alertmanager':
   #   'auth_identity'   => $gmail_account,
   #   'auth_password'   => $gmail_auth_token,
   #   },
-  route         => {
+  route         => {[
     'group_by'        => ['alertname', 'job'],
     'group_wait'      => '30s',
     'group_interval'  => '1m',
     'repeat_interval' => '3h',
-    'receiver'        => 'slack, email',
+    'receiver'        => 'slack',
+]
+  {
+    'group_by'        => ['alertname', 'job'],
+    'group_wait'      => '30s',
+    'group_interval'  => '1m',
+    'repeat_interval' => '3h',
+    'receiver'        => 'email',
   },
   receivers     => [
     { 'name'          => 'email',
