ee3b57c25c60fe4960beaac20970871f48146a39
AnonSS <mysticguy9@yahoo.com>
2021-06-17 08:37:21 -0700

    sx

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -10,7 +10,7 @@ $advertise_ip,
   # Firewall rules configured in hiera
   include prometheus
   include firewalld
-  include prometheus::nginx_vts_exporter
+  # include prometheus::nginx_vts_exporter
   # include prometheus::blackbox_exporter
   class { 'prometheus::blackbox_exporter':
     version => '0.19.0',
