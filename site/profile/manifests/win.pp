9df07b2f6335779dc56fc6f86d7a8b0af7373c93
AnonSS <mysticguy9@yahoo.com>
2021-06-23 09:51:27 -0700

    az

--- site/profile/manifests/win.pp
+++ site/profile/manifests/win.pp
@@ -6,17 +6,18 @@ $nagioscfg,
 ){
   include chocolatey
   include facter
-class { 'prometheus::node_exporter':
-  version            => '1.1.2',
-}
+
 facter::fact { 'symantec_defenition_version':
   value => 'test',
 }
 
-# package { 'windows_exporter':
-#     ensure => '0.16.0',
-#     source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi'
-# }
+package { 'windows_exporter':
+    ensure => '0.16.0',
+    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi'
+}
+service { 'windows_exporter':
+  ensure => running,
+}
 ##### Note: This process could take over 20 minutes so be patient. #####
 class {'::puppet_agent':
   package_version => '7.6.1',
