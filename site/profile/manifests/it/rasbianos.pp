ad0423f8e744bef963a5eaebe7b977a9cbc61e23
AnonSS <mysticguy9@yahoo.com>
2021-06-24 15:12:52 -0700

    sx

--- site/profile/manifests/it/rasbianos.pp
+++ site/profile/manifests/it/rasbianos.pp
@@ -5,6 +5,7 @@ class profile::it::rasbianos {
   include timezone
   include accounts
   include network
+  include php
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
 'acpid', 'wget', 'nmap', 'ifupdown-extra']:
