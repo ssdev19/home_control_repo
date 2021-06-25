58400f9b604167c018a98cdcb546c20a70afb4c6
AnonSS <mysticguy9@yahoo.com>
2021-06-25 13:03:32 -0700

    az

--- site/profile/manifests/it/grafana.pp
+++ site/profile/manifests/it/grafana.pp
@@ -32,4 +32,10 @@ $ldaphost_hide,
       },
   }
 }
+  firewalld_port { 'Grafana Main Port':
+    ensure   => present,
+    port     => '3000',
+    protocol => 'tcp',
+    require  => Service['firewalld'],
+  }
 }
