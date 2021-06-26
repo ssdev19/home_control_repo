bc6f22ac7c4954cdbbe57f4a972bc8f2bec8c76f
AnonSS <mysticguy9@yahoo.com>
2021-06-26 09:41:47 -0700

    az

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -44,7 +44,7 @@ file { '/root/secretfile.cfg':
 
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
-'acpid', 'wget', 'nmap', 'foreman-telemetry', 'prometheus-client']:
+'acpid', 'wget', 'nmap' ]:
 ensure => installed,
 }
 }
