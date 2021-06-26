77d6b3995bc39dd0021738987c02011708e13c53
AnonSS <mysticguy9@yahoo.com>
2021-06-26 15:01:41 -0700

    as

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -35,7 +35,7 @@ class profile::core::common (
 #  }
 class { 'prometheus::node_exporter':
   version       => '1.1.2',
-  extra_options => '--collector.systemd',
+  extra_options => '--collector.systemd \ --collector.process',
   # collectors_disable => ['loadavg', 'mdadm'],
 }
   class {'::puppet_agent':
