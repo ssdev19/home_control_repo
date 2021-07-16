44724196f9998d1414fcb4c9f59de71fa5666ca3
AnonSS <mysticguy9@yahoo.com>
2021-07-16 12:01:41 -0700

    xs

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -20,15 +20,15 @@ $advertise_ip,
         'prober'  => 'http',
         'timeout' => '7s',
         'http'    => {
-          'valid_status_codes'    => [],
+          'valid_status_codes'    => [200],
           'method'                => 'GET',
           'preferred_ip_protocol' => 'ipv4',
         }
       },
-      'tcp_connect' => {
-        'proper'  => 'tcp',
-        'timeout' => '7s',
-      },
+      # 'tcp_connect' => {
+      #   'proper'  => 'tcp',
+      #   'timeout' => '7s',
+      # },
     }
   }
 $gmail_auth_token = lookup('gmail_auth_token')
