d85c6fc727eb2314b816eeed269448942b5cc051
AnonSS <mysticguy9@yahoo.com>
2021-07-19 09:06:44 -0700

    as

--- site/profile/manifests/it/pi.pp
+++ site/profile/manifests/it/pi.pp
@@ -16,6 +16,7 @@ file { '/root/GoDaddy_Bash_DDNS.sh' :
   # package_version => '6.21.1',
   # }
 class { 'prometheus::node_exporter':
+  init_style         => 'systemd',
   version            => '0.15.0',
   collectors_disable => ['loadavg', 'mdadm'],
   extra_options      => '--collector.systemd \--collector.processes \--collector.meminfo_numa',
