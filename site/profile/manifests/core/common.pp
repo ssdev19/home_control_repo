53c7147eaa0f0f822e8ff0d7d5e8bcaef16d67d0
AnonSS <mysticguy9@yahoo.com>
2021-07-20 07:06:07 -0700

    zz

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -50,9 +50,9 @@ class { 'prometheus::node_exporter':
     content => "this string will be encrypted in your catalog\n".node_encrypt::secret
   }
 
-  Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-  'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
-  'acpid', 'wget', 'nmap' ]:
-  ensure => installed,
-  }
+Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
+'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
+'acpid', 'wget', 'nmap', 'bind-utils', 'iputils', 'traceroute' ]:
+ensure => installed,
+}
 }
