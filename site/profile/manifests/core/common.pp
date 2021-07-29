8beb855a0574c8b3214a85efc6148c95cc927032
AnonSS <mysticguy9@yahoo.com>
2021-07-29 10:30:27 -0700

    zz

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -57,11 +57,12 @@ ensure => installed,
 }
 # *.* @graylog-tuc.lsst.org:5514;RSYSLOG_SyslogProtocol23Format
 syslog { "my test":
-  ensure      => present,
-  facility    => "*",
-  level       => "*",
-  action_type => "file",
-  action      => "/var/log/test.log",
-  provider    => "rsyslog",
+  ensure          => present,
+  facility        => "*",
+  level           => "*",
+  action_type     => "hostname",
+  action_port     => "514",
+  action_protocol => "udp",
+  action          => "tuc.lsst.org",
 }
 }
