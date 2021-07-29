fa3855353e7fa752fa76b99bb8f6ba9320a9702e
AnonSS <mysticguy9@yahoo.com>
2021-07-29 10:14:41 -0700

    az

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -61,7 +61,7 @@ syslog { "my test":
   facility        => "*",
   level           => "*",
   action_type     => "hostname",
-  action_port     => "514",
+  action_port     => "514 \n",
   action_protocol => "udp",
   action          => "tuc.lsst.org",
 }
