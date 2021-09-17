9aec79cf1dbe0c3d47b82dd8e06fdd0f3a24182a
AnonSS <mysticguy9@yahoo.com>
2021-09-17 12:43:19 -0700

    upgrade puppet agend 6.24

--- site/profile/manifests/core/common.pp
+++ site/profile/manifests/core/common.pp
@@ -43,7 +43,7 @@ class { 'prometheus::node_exporter':
   # collectors_disable => ['loadavg', 'mdadm'],
 }
   class {'::puppet_agent':
-    package_version => '6.21.1',
+    package_version => '6.24.0',
   }
 
   file { '/root/secretfile.cfg':
