3abc3f0e17e0e0dd3825b12f2c897e59993345db
AnonSS <mysticguy9@yahoo.com>
2021-11-18 13:35:03 -0700

    p

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -5,11 +5,17 @@ $pwd_encrypt,
 ){
   # include node_exporter
   # include prometheus::node_exporter
-  include prometheus::process_exporter
-    package { 'toml':
-    ensure   => present,
-    provider => 'puppetserver_gem',
-    }
+  # include prometheus::process_exporter
+  #   package { 'toml':
+  #   ensure   => present,
+  #   provider => 'puppetserver_gem',
+  #   }
+class { 'prometheus::node_exporter':
+  version       => '1.1.2',
+  extra_options => '--collector.systemd \--collector.processes \--collector.meminfo_numa',
+  # collectors_disable => ['loadavg', 'mdadm'],
+  # extra_options      => '--collector.ntp.server ntp1.orange.intra',
+}
 # firewall config
   # class { 'firewalld':
   #   service_ensure => stopped,
