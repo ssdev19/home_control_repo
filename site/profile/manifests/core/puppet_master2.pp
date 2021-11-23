753406a02f89700d23305529569f73ae3bd8e089
AnonSS <mysticguy9@yahoo.com>
2021-11-23 09:33:25 -0700

    xc

--- site/profile/manifests/core/puppet_master2.pp
+++ site/profile/manifests/core/puppet_master2.pp
@@ -4,12 +4,12 @@ $psswrd_encrypt,
 $pwd_encrypt,
 ){
   include foreman
-  # include foreman::cli
+  include foreman::cli
   # include foreman::compute::libvirt
   # include foreman::compute::vmware
-  # include foreman::plugin::remote_execution
-  # include foreman::plugin::tasks
-  # include foreman_proxy
+  include foreman::plugin::remote_execution
+  include foreman::plugin::tasks
+  include foreman_proxy
   # include foreman_proxy::plugin::dns::route53
   # include foreman_proxy::plugin::dynflow
   # include foreman_proxy::plugin::remote_execution::ssh
