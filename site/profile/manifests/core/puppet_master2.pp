fcb3bb92d47c16959e65bf41a61aeb80e4e6b8d2
AnonSS <mysticguy9@yahoo.com>
2021-11-23 10:42:52 -0700

    qq

--- site/profile/manifests/core/puppet_master2.pp
+++ site/profile/manifests/core/puppet_master2.pp
@@ -3,7 +3,7 @@ class profile::core::puppet_master2 (Sensitive[String]
 $psswrd_encrypt,
 $pwd_encrypt,
 ){
-  Package { [ 'devtoolset-10',
+  Package { [ 'devtoolset-101',
     'rh-ruby26-ruby-devel', ]:
 ensure => present,
 }
