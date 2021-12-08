114da72161bfa0d26b1ee106bc16b942191f1df2
AnonSS <mysticguy9@yahoo.com>
2021-12-08 10:44:57 -0700

    sx

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -2,6 +2,7 @@
 class profile::it::pwm {
   class { 'java' :
   package => 'java-1.8.0-openjdk-devel',
+  ensure  => absent,
   }
   tomcat::install { '/opt/tomcat10':
   source_url => 'https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.4/bin/apache-tomcat-10.0.4.tar.gz'
