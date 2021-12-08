661f3ac4c7c02b8a3eefa9a60fbb27708f9e6796
AnonSS <mysticguy9@yahoo.com>
2021-12-08 11:32:46 -0700

    xx

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -8,6 +8,16 @@ class profile::it::pwm {
   tomcat::install { '/opt/tomcat10':
   source_url => 'https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.4/bin/apache-tomcat-10.0.4.tar.gz'
   }
+# file { '/opt/tomcat10/webapps/pwm/' :
+#   ensure  => directory,
+# }
+    vcsrepo { '/opt/tomcat10/webapps/pwm/':
+    ensure             => present,
+    provider           => git,
+    revision           => 'b06b257c6fa13049a72e2c915017996bbdb43d11',
+    source             => 'https://github.com/pwm-project/pwm.git',
+    keep_local_changes => true,
+  }
   package { 'maven':
     ensure => 'present',
   }
