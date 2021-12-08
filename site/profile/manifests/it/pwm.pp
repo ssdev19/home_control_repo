f4b5cdeaadf51683bc7391841e51f8a27dae0d47
AnonSS <mysticguy9@yahoo.com>
2021-12-08 14:20:55 -0700

    x

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -19,11 +19,16 @@ class profile::it::pwm {
     source             => 'https://github.com/pwm-project/pwm.git',
     # keep_local_changes => true,
   }
-    class { 'maven::maven':
-      version => '3.8.4', # version to install
-      ensure_packages => ('maven'),
-      ensure => 'present',
+  # Install Maven
+  class { "maven::maven":
+    version => "3.8.4", # version to install
+    # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
+    repo => {
+    url => 'http://apache.rediris.es/maven/maven-3',
+      #username => "",
+      #password => "",
     }
+  }
   # package { ['maven', 'firefox']:
   #   ensure => 'absent',
   # }
