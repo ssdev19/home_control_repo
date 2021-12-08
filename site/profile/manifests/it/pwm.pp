86d8750151b851acf81901b5cfdb456f5a179073
AnonSS <mysticguy9@yahoo.com>
2021-12-08 10:15:31 -0700

    add java and tomcat

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -1,4 +1,10 @@
 # pwm 
 class profile::it::pwm {
+  class { 'java' :
+  package => 'java-1.8.0-openjdk-devel',
+  }
+  tomcat::install { '/opt/tomcat10':
+  source_url => 'https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.4/bin/apache-tomcat-10.0.4.tar.gz'
+  }
 
 }
