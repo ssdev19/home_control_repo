4dffcbe17da2bcda1a43695df07038a0a9005bf0
AnonSS <mysticguy9@yahoo.com>
2021-12-08 11:10:46 -0700

    mvn

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -1,8 +1,5 @@
 # pwm 
 class profile::it::pwm {
-  package { 'maven':
-    ensure => 'present',
-  }
   java::adopt { 'jdk11' :
   ensure  => 'present',
   version => '11',
@@ -11,4 +8,10 @@ class profile::it::pwm {
   tomcat::install { '/opt/tomcat10':
   source_url => 'https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.4/bin/apache-tomcat-10.0.4.tar.gz'
   }
+  package { 'maven':
+    ensure => 'present',
+  }
+  exec { 'mvn package':
+    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    command => 'mvn package',
 }
