6f55717f72d354a1a8c54a7b023580df808b7612
AnonSS <mysticguy9@yahoo.com>
2021-12-08 12:02:19 -0700

    s

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -8,9 +8,6 @@ class profile::it::pwm {
   tomcat::install { '/opt/tomcat10':
   source_url => 'https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.4/bin/apache-tomcat-10.0.4.tar.gz'
   }
-# file { '/opt/tomcat10/webapps/pwm/' :
-#   ensure  => directory,
-# }
     vcsrepo { '/opt/tomcat10/webapps/pwm/':
     ensure             => present,
     provider           => git,
@@ -21,8 +18,8 @@ class profile::it::pwm {
   package { 'maven':
     ensure => 'present',
   }
-  # exec { 'mvn package':
-  #   path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-  #   command => 'mvn package',
-  # }
+  exec { 'mvn package':
+    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    command => 'mvn /opt/tomcat10/webapps/pwm/',
+  }
 }
