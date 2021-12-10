d141236b74f9f993e17543899e0129b7b3aad750
AnonSS <mysticguy9@yahoo.com>
2021-12-10 08:39:59 -0700

    cc

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -29,7 +29,7 @@ class profile::it::pwm {
 #   cleanup       => true,
 # }
 $install_path        = '/opt/maven'
-$package_name        = 'apache-maven-bin.tar.gz'
+$package_name        = 'apache-maven'
 $package_ensure      = '3.8.4'
 $repository_url      = 'https://dlcdn.apache.org/maven/maven-3'
 $archive_name        = "${package_name}-${package_ensure}-bin.tar.gz"
