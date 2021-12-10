a49c5964b8469de1fe5026ab69afb2ca97528366
AnonSS <mysticguy9@yahoo.com>
2021-12-10 14:28:54 -0700

    ss

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -70,7 +70,7 @@ archive { $archive_name:
   #   command => 'export _JAVA_OPTIONS="-Xmx1g"',
   # }
   exec { 'mvn package':
-    path    => [ '/opt/maven/bin' ],
+    path    => [ '/opt/apache-maven-3.8.4/bin/' ],
     cwd     => '/opt/tomcat10/webapps/pwm',
     command => 'mvn package',
   }
