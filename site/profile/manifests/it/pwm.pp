e973677ddf1102776ab1846c81a1440c0bd082e6
AnonSS <mysticguy9@yahoo.com>
2021-12-11 10:22:35 -0700

    s

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -47,7 +47,7 @@ archive { $archive_name:
 }
   exec { 'create symbolic link for maven':
     path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
-    command => 'ln –s apache-maven-3.8.4 /opt/maven',
+    command => 'sudo ln –s apache-maven-3.8.4 /opt/maven',
     onlyif  => 'test ! -f /opt/maven/README.txt'
   }
   file { '/etc/profile.d/maven.sh':
