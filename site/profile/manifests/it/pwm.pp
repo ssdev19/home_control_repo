f3144844139ab7c44c405c737a67186acb58f244
AnonSS <mysticguy9@yahoo.com>
2021-12-11 10:43:41 -0700

    ss

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -52,7 +52,7 @@ archive { $archive_name:
   # }
     file { '/opt/apache-maven-3.8.4/':
     ensure => 'link',
-    target => '/opt/maven',
+    target => '/opt/maven/',
   }
   file { '/etc/profile.d/maven.sh':
   ensure  => file,
