07e2a80dd53e22fc49acc30b1191ed82ce8f943b
AnonSS <mysticguy9@yahoo.com>
2021-12-11 20:10:21 -0700

    sd

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -56,11 +56,11 @@ archive { $archive_name:
     recurse => true,
     links   => follow,
   }
-  ->  file { '/opt/maven/':
-        ensure => 'link',
-        target => '/opt/maven/',
-        force  => true,
-      }
+  # ->  file { '/opt/maven/':
+  #       ensure => 'link',
+  #       target => '/opt/maven/',
+  #       force  => true,
+  #     }
   file { '/etc/profile.d/maven.sh':
   ensure  => file,
   content => epp('profile/it/maven.epp'),
