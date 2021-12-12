dda95b9982ca6e1af6e1a3d8acd683e6befd079b
AnonSS <mysticguy9@yahoo.com>
2021-12-11 20:26:25 -0700

    ss

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -51,12 +51,12 @@ archive { $archive_name:
   #   onlyif  => 'test ! -f /opt/maven/README.txt'
   # }
 
-  file { '/opt/maven/':
-    ensure  => 'directory',
-    source  => '/opt/apache-maven-3.8.4/',
-    recurse => true,
-    links   => follow,
-  }
+  # file { '/opt/maven/':
+  #   ensure  => 'directory',
+  #   source  => '/opt/apache-maven-3.8.4/',
+  #   recurse => true,
+  #   links   => follow,
+  # }
   ->  file { '/opt/maven/':
         ensure => 'link',
         target => '/opt/apache-maven-3.8.4/',
