269a838a33ef04a0a887264f3047dcd726812d9b
AnonSS <mysticguy9@yahoo.com>
2021-12-14 10:12:31 -0700

    sd

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -3,14 +3,14 @@ class profile::it::pwm {
   include firewalld
 
   # Installs Java in '/usr/java/jdk-11.0.2+9/bin/'
-class { 'java':
-  distribution => 'jdk',
-}
-  # java::adopt { 'jdk11' :
-  # ensure  => 'present',
-  # version => '11',
-  # java    => 'jdk',
-  # }
+  class { 'java':
+    distribution => 'jdk',
+  }
+  java::adopt { 'jdk11' :
+  ensure  => 'present',
+  version => '11',
+  java    => 'jdk',
+  }
   # export _JAVA_OPTIONS="-Xmx1g"
   $mem = '-Xmx1g'
   exec { 'set java heap size ':
