826a6ea17620303e39bd48ef91a7455ff1e6e1b0
AnonSS <mysticguy9@yahoo.com>
2021-12-15 15:48:12 -0700

    sx

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -94,6 +94,11 @@ ensure => installed,
     path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
     command => 'sudo -s source /etc/profile.d/maven.sh', # Source needs to run in shell
   }
+  $pwm_applicationpath = '/opt/tomcat9/webapps/pwm/'
+  exec { 'set pwm application path':
+    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    command => "sudo -s export PWM_APPLICATIONPATH=${pwm_applicationpath}",
+  }
 # "mvn package" should be run from '/opt/tomcat9/webapps/pwm' manually
 # Having puppet execute, times out.
   # exec { 'mvn package':
