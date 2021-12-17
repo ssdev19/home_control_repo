29a48c68e09d4eb2cc00cd8c6746774c85f453f9
AnonSS <mysticguy9@yahoo.com>
2021-12-17 12:01:32 -0700

    sx

--- site/profile/manifests/it/pwm.pp
+++ site/profile/manifests/it/pwm.pp
@@ -25,18 +25,20 @@ ensure => installed,
     command => 'sudo -s export PATH=/usr/java/jdk-11.0.2+9/bin:$PATH',
   }
   #Install tomcat
-  tomcat::install { '/opt/tomcat9':
-  source_url => 'https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.56/bin/apache-tomcat-9.0.56.tar.gz'
+  tomcat::install { 'tomcat9':
+  source_url     => 'https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.56/bin/apache-tomcat-9.0.56.tar.gz',
+  allow_insecure => true,
+  # user           => 'tomcatuser',
   }
-  tomcat::instance { 'default':
-  catalina_home  => '/opt/tomcat9',
+  # tomcat::instance { 'default':
+  # catalina_home  => '/opt/tomcat9',
   # manage_service => false,
-  }
-  tomcat::config::server::tomcat_users { 'admin':
-    password      => 'tomcatpass',
-    roles         => ['admin-gui, manager-gui, manager-script'],
-    catalina_base => '/opt/tomcat9',
-  }
+  # }
+  # tomcat::config::server::tomcat_users { 'admin':
+  #   password      => 'tomcatpass',
+  #   roles         => ['admin-gui, manager-gui, manager-script'],
+  #   catalina_base => '/opt/tomcat9',
+  # }
 #   tomcat::war { 'pwm.war':
 #   catalina_base => '/opt/tomcat9/webapps/',
 #   war_source    => '/opt/tomcat9/webapps/pwm.war',
@@ -61,54 +63,54 @@ ensure => installed,
   #   source             => 'https://github.com/pwm-project/pwm.git',
   #   # keep_local_changes => true,
   # }
-  archive { '/opt/tomcat9/webapps/pwm.war':
-    ensure   => present,
-    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
-    provider => 'wget',
-  }
-# Maven installation
-  $install_path        = '/opt'
-  $package_name        = 'apache-maven'
-  $package_ensure      = '3.8.4'
-  $repository_url      = 'https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries'
-  $archive_name        = "${package_name}-${package_ensure}-bin.tar.gz"
-  $maven_package_source = "${repository_url}/${archive_name}"
+#   archive { '/opt/tomcat9/webapps/pwm.war':
+#     ensure   => present,
+#     source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
+#     provider => 'wget',
+#   }
+# # Maven installation
+#   $install_path        = '/opt'
+#   $package_name        = 'apache-maven'
+#   $package_ensure      = '3.8.4'
+#   $repository_url      = 'https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries'
+#   $archive_name        = "${package_name}-${package_ensure}-bin.tar.gz"
+#   $maven_package_source = "${repository_url}/${archive_name}"
 
-  archive { $archive_name:
-    path         => "/tmp/${archive_name}",
-    source       => $maven_package_source,
-    extract      => true,
-    extract_path => $install_path,
-    creates      => "${install_path}/${package_name}-${package_ensure}",
-    cleanup      => true,
-    # require      => File[$install_path],
-  }
-# Creates maven symlink that points to '/opt/apache-maven-3.8.4/'
-  file { '/opt/maven/':
-    ensure => 'link',
-    target => '/opt/apache-maven-3.8.4/',
-    # force  => true,
-  }
-# Creates maven.sh file
-  file { '/etc/profile.d/maven.sh':
-  ensure  => file,
-  content => epp('profile/it/maven.epp'),
-  }
-  # Make maven.sh executable
-  exec { 'make maven.sh executable':
-    path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
-    command => 'chmod +x /etc/profile.d/maven.sh',
-  }
-  # If puppet fails to execute run this manually: source /etc/profile.d/maven.sh
-  exec { 'Load the environment variable':
-    path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
-    command => 'sudo -s source /etc/profile.d/maven.sh', # Source needs to run in shell
-  }
-  $pwm_applicationpath = '/opt/tomcat9/webapps/pwm'
-  exec { 'set pwm application path':
-    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    command => "sudo -s export PWM_APPLICATIONPATH=${pwm_applicationpath}",
-  }
+#   archive { $archive_name:
+#     path         => "/tmp/${archive_name}",
+#     source       => $maven_package_source,
+#     extract      => true,
+#     extract_path => $install_path,
+#     creates      => "${install_path}/${package_name}-${package_ensure}",
+#     cleanup      => true,
+#     # require      => File[$install_path],
+#   }
+# # Creates maven symlink that points to '/opt/apache-maven-3.8.4/'
+#   file { '/opt/maven/':
+#     ensure => 'link',
+#     target => '/opt/apache-maven-3.8.4/',
+#     # force  => true,
+#   }
+# # Creates maven.sh file
+#   file { '/etc/profile.d/maven.sh':
+#   ensure  => file,
+#   content => epp('profile/it/maven.epp'),
+#   }
+#   # Make maven.sh executable
+#   exec { 'make maven.sh executable':
+#     path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
+#     command => 'chmod +x /etc/profile.d/maven.sh',
+#   }
+#   # If puppet fails to execute run this manually: source /etc/profile.d/maven.sh
+#   exec { 'Load the environment variable':
+#     path    => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ],
+#     command => 'sudo -s source /etc/profile.d/maven.sh', # Source needs to run in shell
+#   }
+#   $pwm_applicationpath = '/opt/tomcat9/webapps/pwm'
+#   exec { 'set pwm application path':
+#     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+#     command => "sudo -s export PWM_APPLICATIONPATH=${pwm_applicationpath}",
+#   }
 # "mvn package" should be run from '/opt/tomcat9/webapps/pwm' manually
 # to see errors.
   # exec { 'mvn package':
