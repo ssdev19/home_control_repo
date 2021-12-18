e272cf1b7889ce894100e33a118247eeb37a91b5
AnonSS <mysticguy9@yahoo.com>
2021-12-18 10:42:02 -0700

    war

--- site/profile/manifests/it/pwm3.pp
+++ site/profile/manifests/it/pwm3.pp
@@ -73,11 +73,11 @@ ensure => installed,
   #   source             => 'https://github.com/pwm-project/pwm.git',
   #   # keep_local_changes => true,
   # }
-  # archive { '/opt/tomcat8/webapps/pwm.war':
-  #   ensure   => present,
-  #   source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
-  #   provider => 'wget',
-  # }
+  archive { '/opt/tomcat8/webapps/pwm.war':
+    ensure   => present,
+    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
+    provider => 'wget',
+  }
 # # Maven installation
 #   $install_path        = '/opt'
 #   $package_name        = 'apache-maven'
