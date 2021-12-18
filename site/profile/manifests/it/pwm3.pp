d322aec22a352f2e7c0d39fc02ee63433e274adf
AnonSS <mysticguy9@yahoo.com>
2021-12-18 15:04:55 -0700

    az

--- site/profile/manifests/it/pwm3.pp
+++ site/profile/manifests/it/pwm3.pp
@@ -29,13 +29,13 @@ class profile::it::pwm3 {
   tomcat::instance { 'default':
   catalina_home  => '/opt/tomcat9',
   catalina_base  => '/opt/tomcat9',
-  manage_service => false,
+  manage_service => true,
   }
   # Removes entry in: /opt/tomcat9/webapps/manager/META-INF/context.xml
   # For some reason it does not remove it, had to do it manually
   tomcat::config::context::manager { 'org.apache.catalina.valves.RemoteAddrValve':
   ensure        => 'absent',
-  catalina_base => '/opt/tomcat9/',
+  catalina_base => '/opt/tomcat9',
   }
   tomcat::config::server::tomcat_users { 'tomcatuser':
     password      => 'tomcatpass',
