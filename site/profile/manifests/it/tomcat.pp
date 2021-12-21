a259c9d751bcb9229520c4d79979e6ac4ae1777c
AnonSS <mysticguy9@yahoo.com>
2021-12-21 10:18:53 -0700

    aa

--- site/profile/manifests/it/tomcat.pp
+++ site/profile/manifests/it/tomcat.pp
@@ -1,7 +1,7 @@
 # tomcat 
 class profile::it::tomcat {
   tomcat::install { '/opt/tomcat':
-  source_url     => 'https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.73/bin/apache-tomcat-8.5.73.tar.gz',
+  source_url     => 'https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.56/bin/apache-tomcat-9.0.56.tar.gz',
   }
   tomcat::instance { 'default':
   catalina_home  => '/opt/tomcat',
