6df1a23476dd2f1c5c39056679a394cd434c7c65
AnonSS <mysticguy9@yahoo.com>
2021-12-21 09:33:53 -0700

    df

--- site/profile/manifests/it/pwm3.pp
+++ site/profile/manifests/it/pwm3.pp
@@ -19,10 +19,10 @@ class profile::it::pwm3 {
   #   path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
   #   command => "sudo -s export _JAVA_OPTIONS=${mem}",
   # }
-  exec { 'set java path':
-    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    command => 'sudo -s export PATH=/usr/java/jdk8u202-b08-jre/bin:$PATH',
-  }
+  # exec { 'set java path':
+  #   path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  #   command => 'sudo -s export PATH=/usr/java/jdk8u202-b08-jre/bin:$PATH',
+  # }
   # give permission first: chmod 777 /opt/tomcat/
   #Install tomcat
   tomcat::install { '/opt/tomcat':
