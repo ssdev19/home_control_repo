dce00408609e12466d3112f7a2f0a6616254f30e
AnonSS <mysticguy9@yahoo.com>
2021-12-25 08:49:28 -0700

    dd

--- site/profile/manifests/it/tomcat3.pp
+++ site/profile/manifests/it/tomcat3.pp
@@ -103,7 +103,7 @@ $distribution,
     # checksum_value => $keystore_checksum,
   } ->
 
-  tomcat::config::server::connector { "${tomcat_instance}-https":
+  tomcat::config::server::connector { "default-https":
     catalina_base         => $catalina_base,
     port                  => 8080,
     protocol              =>'TLSv1.2', # $http_version,
