12568067e98099ec737ac3273a6ca694eec0bca8
AnonSS <mysticguy9@yahoo.com>
2021-12-28 09:15:06 -0700

    df

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -14,14 +14,8 @@ class profile::default (
   #   ensure  => present,
   #   timeout => 3300,
   # }
-  class { '::openssl':
-    package_ensure         => present,
-    ca_certificates_ensure => present,
-    openssl_version_raw    => 'openssl-3.0.1',
-  }
-
 Package { [ 'git', 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim',
+'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
 'acpid', 'wget', 'nmap', 'bind-utils', 'iputils', 'traceroute' ]:
 ensure => installed,
 }
