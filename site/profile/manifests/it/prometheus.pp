762203b9ed0235234071f4151a9f85c5b83ddda1
AnonSS <mysticguy9@yahoo.com>
2022-01-21 09:10:04 -0700

    ss

--- site/profile/manifests/it/prometheus.pp
+++ site/profile/manifests/it/prometheus.pp
@@ -80,7 +80,7 @@ class { 'prometheus::alertmanager':
         {
           'api_url'       => unwrap($slackapi_hide),
           'channel'       => '#monitoring',
-          'icon_url'      => 'https://avatars3.githubusercontent.com/u/3380462',
+          'icon_url'      => 'https://imgur.com/ZqYqeEC',
           'username'      => unwrap($slackuser_hide),
           'title'         => '{{ template "custom_title" . }}',
           'text'          => '{{ template "custom_slack_message" . }}',
