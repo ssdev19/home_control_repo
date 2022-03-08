1d752a770be32bdd4262912bc33fe8922684eb78
AnonSS <mysticguy9@yahoo.com>
2022-03-08 15:41:54 -0700

    sd

--- site/profile/manifests/it/rasbianos.pp
+++ site/profile/manifests/it/rasbianos.pp
@@ -8,9 +8,19 @@ class profile::it::rasbianos {
   include '::php'
   include '::mysql::server'
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
-'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute', 'unzip' ]:
-ensure => installed,
+  'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
+  'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute', 'unzip' ]:
+  ensure => installed,
+}
+class { 'chrony':
+  servers => [ 'time-a-g.nist.gov', 'time-a-wwv.nist.gov', 'time.nist.gov' ],
+}
+# config: /etc/systemd/system/node_exporter.service
+class { 'prometheus::node_exporter':
+  version       => '1.1.2',
+  extra_options => '--collector.systemd \--collector.processes \--collector.meminfo_numa',
+  # collectors_disable => ['loadavg', 'mdadm'],
+  # extra_options      => '--collector.ntp.server ntp1.orange.intra',
 }
 # class { 'phpmyadmin': }
 # $db_password = lookup('db_password')
