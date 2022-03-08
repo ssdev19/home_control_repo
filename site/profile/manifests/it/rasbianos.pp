c20a4882c611ecc2f2a1e24ba05face70f44b24a
AnonSS <mysticguy9@yahoo.com>
2022-03-08 15:27:16 -0700

    sd

--- site/profile/manifests/it/rasbianos.pp
+++ site/profile/manifests/it/rasbianos.pp
@@ -9,17 +9,17 @@ class profile::it::rasbianos {
   include '::mysql::server'
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
-'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute' ]:
+'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute', 'unzip' ]:
 ensure => installed,
 }
 # class { 'phpmyadmin': }
-$db_password = lookup('db_password')
-wordpress::instance { '/var/www/wpbd':
-  wp_owner    => 'wordpress',
-  wp_group    => 'wordpress',
-  db_user     => 'wordpress',
-  db_name     => 'wpbd',
-  db_password => $db_password,
-}
+# $db_password = lookup('db_password')
+# wordpress::instance { '/var/www/wpbd':
+#   wp_owner    => 'wordpress',
+#   wp_group    => 'wordpress',
+#   db_user     => 'wordpress',
+#   db_name     => 'wpbd',
+#   db_password => $db_password,
+# }
 
 }
