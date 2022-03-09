c3d98dd215108575d73a072ce9c95d91ac925951
AnonSS <mysticguy9@yahoo.com>
2022-03-09 10:14:16 -0700

    ss

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -22,7 +22,8 @@ class profile::default (
   # }
 Package { [ 'git', 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
-'acpid', 'wget', 'nmap', 'bind-utils', 'iputils', 'traceroute' ]:
+'acpid', 'wget', 'nmap', 'bind-utils', 'iputils', 'traceroute', 
+'unzip', 'net-tools' ]:
 ensure => installed,
 }
 class { 'chrony':
