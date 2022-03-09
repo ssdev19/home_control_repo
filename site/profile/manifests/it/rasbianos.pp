a011ace7e26cf236bc364ea79b8d022909081d24
AnonSS <mysticguy9@yahoo.com>
2022-03-09 10:16:53 -0700

    sx

--- site/profile/manifests/it/rasbianos.pp
+++ site/profile/manifests/it/rasbianos.pp
@@ -9,7 +9,8 @@ class profile::it::rasbianos {
   include '::mysql::server'
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
   'bash-completion', 'sudo', 'screen', 'vim', 'openssl',
-  'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute', 'unzip' ]:
+  'acpid', 'wget', 'nmap', 'ifupdown-extra', 'traceroute', 
+  'unzip', 'net-tools' ]:
   ensure => installed,
 }
 class { 'chrony':
