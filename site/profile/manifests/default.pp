47ffabc4d709f7e108a8a712a91808ee04c17704
anonss <mysticguy9@yahoo.com>
2022-04-12 08:47:52 -0700

    sd

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -21,7 +21,7 @@ class profile::default (
   #   timeout => 3300,
   # }
 Package { [ 'git', 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
+'bash-completion', 'sudo', 'vim', 'openssl', 'openssl-devel',
 'acpid', 'wget', 'nmap', 'bind-utils', 'iputils', 'traceroute',
 'unzip', 'net-tools' ]:
 ensure => installed,
@@ -91,45 +91,4 @@ file { '/etc/hosts.allow' :
   ensure  => file,
   content => $allowhosts,
 }
-
-  # Firewall and security measurements
-  # file_line { 'SELINUX=permissive':
-  #   path  => '/etc/selinux/config',
-  #   line  => 'SELINUX=enforce',
-  #   match => '^SELINUX=+',
-  # }
-  $firewall_default_zone = lookup('firewall_default_zone')
-
-  # class { 'firewalld':
-  #   service_ensure => lookup('firewalld_status'),
-  #   default_zone   => $firewall_default_zone,
-  # }
-
-  # firewalld_zone { $firewall_default_zone:
-  # ensure  => present,
-  # target  => lookup('firewall_default_target'),
-  # sources => lookup('firewall_default_sources')
-  # }
-
-  # firewalld_service { 'Enable SSH':
-  # ensure  => 'present',
-  # service => 'ssh',
-  # }
-
-# 	firewalld_service { 'Enable DHCP':
-# 		ensure  => 'present',
-# 		service => 'dhcpv6-client',
-# 	}
-
-  # exec{'enable_icmp':
-  #   provider => 'shell',
-  #   command  => '/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload',
-  #   require  => Class['firewalld'],
-  #   onlyif   => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
-  # }
-  Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-  'bash-completion', 'sudo', 'vim', 'openssl', 'openssl-devel',
-  'acpid', 'wget', 'nmap']:
-  ensure => installed,
-  }
 }
