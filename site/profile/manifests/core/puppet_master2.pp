fc9fa0ebdf4d238ba4bf15db844855523692dcd7
anonss <mysticguy9@yahoo.com>
2022-04-16 12:25:34 -0700

    ss

--- site/profile/manifests/core/puppet_master2.pp
+++ site/profile/manifests/core/puppet_master2.pp
@@ -76,6 +76,6 @@ file {
 # }
   package{ 'puppetserver':
     ensure => installed,
-    source => 'https://yum.puppet.com/puppet7-release-el-8.noarch.rpm',
+    source => 'https://yum.puppet.com/puppet7/puppet7-release-el-8.noarch.rpm',
   }
 }
